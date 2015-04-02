4b1121c5def433e48c8bf2b2a5436e0302ca53c7
xiaofei <alibaba22001@163.com>
2015-04-03 01:34:04 +0800

    update lzma test code

--- lzma/mylzma.cpp
+++ lzma/mylzma.cpp
@@ -3,54 +3,166 @@
 #include "optparse.h"
 #include <string.h>
 #include <errno.h>
+#include "C/LzmaLib.h"
 
-int mycompress(std::string in, std::string out, int level, int lc, int lp, int pb, int fb) {
-	size_t destLen = filesize*2;
-	unsigned char *psrcRead;
-	unsigned char *pLzma;
+int mycompress(std::string in, std::string out, int level, unsigned dictsize, int lc, int lp, int pb, int fb) {
+	int ret = 0;
+	unsigned char *src_buffer;
+	unsigned char *compress_buffer;
+	size_t filesize;
+	unsigned char prop[5];
+	size_t prop_size = 5;
+	size_t out_size;
+	int numthreads = 2;/* 1 or 2, default = 2 */
 
-	std::ifstream ifs(in);
-	std::ofstream ofs(out);
 
-	if(ifs.good()) {
-		ifs.seekg (0, ifs.end);
-		int length = ifs.tellg();
-		ifs.seekg (0, ifs.beg);
-
-		unsigned char *psrcRead = new unsigned char[length]; //原始文件数据
-		unsigned char *pLzma = new unsigned char[length]; //存放压缩数据
-		ifs.read(psrcRead, length);
-		ifs.close();
+	std::ifstream ifs(in.c_str());
+	std::ofstream ofs(out.c_str());
+	if(!ifs.good()) {
+		printf("[%s:%s:%d]", __FILE__, __PRETTY_FUNCTION__, __LINE__);
+		printf("%s:%m\n", strerror(errno));
+		ret = -1;
+		goto l1;
 	}
 
-	unsigned char prop[5];
-	size_t sizeProp = 5;
+	ifs.seekg (0, ifs.end);
+	filesize = ifs.tellg();
+	ifs.seekg (0, ifs.beg);
+	if(!ifs.good()) {
+		printf("[%s:%s:%d]", __FILE__, __PRETTY_FUNCTION__, __LINE__);
+		printf("%s:%m\n", strerror(errno));
+		ret = -1;
+		goto l2;
+	}
 
-	if (SZ_OK != LzmaCompress(pLzma,&destLen,psrcRead,filesize,prop,&sizeProp,5,(1<<24),3,0,2,32,2)) {//出错了
+	src_buffer = new unsigned char[filesize]; //原始文件数据
+	if(src_buffer == NULL) {
+		ret = -1;
+		goto l3;
+	}
+	compress_buffer = new unsigned char[filesize * 2]; //存放压缩数据
+	if(compress_buffer == NULL) {
+		ret = -1;
+		goto l4;
+	}
+
+	ifs.read((char *)src_buffer, filesize);
+	if(!ifs.good()) {
+		printf("[%s:%s:%d]", __FILE__, __PRETTY_FUNCTION__, __LINE__);
+		printf("%s:%m\n", strerror(errno));
+		ret = -1;
+		goto l4;
+	}
+
+	if (SZ_OK != LzmaCompress(compress_buffer, &out_size, src_buffer, filesize, prop, &prop_size, level, dictsize, lc, lp, pb, fb, numthreads)) {//出错了
 		printf("[%s:%s:%d]", __FILE__, __PRETTY_FUNCTION__, __LINE__);
 		printf("%s:%m\n", strerror(errno));
 		printf("压缩时出错！");
-		delete psrcRead;
-		delete pDecomress;
-		delete pLzma;
-		fclose(binfile);
-		return -1;
+		ret = -1;
+		goto l4;
 	}
 
-	FILE *compressfile = fopen("compress.dat","wb+"); //写入压缩后的数据
-	if (compressfile == NULL)
-	{
+	if(ofs.good()) {
+		ofs.write((const char *)compress_buffer, out_size);//写入压缩后的数据
+	}
+
+	if(!ofs.good()) {
 		printf("[%s:%s:%d]", __FILE__, __PRETTY_FUNCTION__, __LINE__);
 		printf("%s:%m\n", strerror(errno));
 		printf("创建文件出错！");
-		delete psrcRead;
-		delete pDecomress;
-		delete pLzma;
-		fclose(binfile);
-		return -1;
+		ret = -1;
+		goto l5;
 	}
-	fwrite(pLzma,sizeof(char),destLen,compressfile);
-	fclose(compressfile);
+	
+
+l5:
+	ofs.close();
+l4:
+	delete [] compress_buffer;
+l3:
+	delete [] src_buffer;
+l2:
+	ifs.close();
+l1:
+	return ret;
+}
+
+int mydecompress(std::string in, std::string out) {
+	int ret = -1;
+
+	unsigned char *src_buffer;
+	unsigned char *decompress_buffer;
+	size_t filesize;
+	size_t out_size;
+	unsigned char prop[5];
+	size_t prop_size = 5;
+
+	std::ifstream ifs(in.c_str());
+	std::ofstream ofs(out.c_str());
+	if(!ifs.good()) {
+		printf("[%s:%s:%d]", __FILE__, __PRETTY_FUNCTION__, __LINE__);
+		printf("%s:%m\n", strerror(errno));
+		ret = -1;
+		goto l1;
+	}
+
+	ifs.seekg (0, ifs.end);
+	filesize = ifs.tellg();
+	ifs.seekg (0, ifs.beg);
+	if(!ifs.good()) {
+		printf("[%s:%s:%d]", __FILE__, __PRETTY_FUNCTION__, __LINE__);
+		printf("%s:%m\n", strerror(errno));
+		ret = -1;
+		goto l2;
+	}
+
+	src_buffer = new unsigned char[filesize]; //原始文件数据
+	if(src_buffer == NULL) {
+		ret = -1;
+		goto l3;
+	}
+	decompress_buffer = new unsigned char[filesize * 10]; //存放压缩数据
+	if(decompress_buffer == NULL) {
+		ret = -1;
+		goto l4;
+	}
+	ifs.read((char *)src_buffer, filesize);
+	if(!ifs.good()) {
+		printf("[%s:%s:%d]", __FILE__, __PRETTY_FUNCTION__, __LINE__);
+		printf("%s:%m\n", strerror(errno));
+		ret = -1;
+		goto l4;
+	}
+
+	if (SZ_OK != LzmaUncompress(decompress_buffer, &out_size, src_buffer, &filesize, prop, prop_size)) {
+		printf("[%s:%s:%d]", __FILE__, __PRETTY_FUNCTION__, __LINE__);
+		printf("%s:%m\n", strerror(errno));
+		printf("解压缩时出错！");
+		ret = -1;
+		goto l4;
+	}
+
+	if(ofs.good()) {
+		ofs.write((char *)decompress_buffer, out_size);//写入压缩后的数据
+	}
+	if(!ofs.good()) {
+		printf("[%s:%s:%d]", __FILE__, __PRETTY_FUNCTION__, __LINE__);
+		printf("%s:%m\n", strerror(errno));
+		printf("创建文件出错！");
+		ret = -1;
+		goto l5;
+	}
+
+l5:
+	ofs.close();
+l4:
+	delete [] decompress_buffer;
+l3:
+	delete [] src_buffer;
+l2:
+	ifs.close();
+l1:
+	return ret;
 }
 
 int main(int argc, char **argv) {
@@ -67,7 +179,7 @@ int main(int argc, char **argv) {
 		ret = -1;
 		return ret;
 	}
-	cp1->add_long_option("level", true, false, 'l', "output file name");
+	cp1->add_long_option("level", true, false, 'l', "0 <= level <= 9, default = 5---for compress");
 	cp1->add_long_option("dictsize", true, false, 'd', "(1 << 12) <= dictSize <= (1 << 27) for 32-bit version\n (1 << 12) <= dictSize <= (1 << 30) for 64-bit version\n default = (1 << 24)---for compress");
 	cp1->add_long_option("lc", true, false, 'c', "0 <= lc <= 8, default = 3---for compress");
 	cp1->add_long_option("lp", true, false, 'p', "0 <= lp <= 4, default = 0---for compress");
@@ -118,82 +230,25 @@ int main(int argc, char **argv) {
 	}
 
 	if(lcp.get_curparser() == cp1) {
-		int level, lc, lp, pb, fb;
-		level = cp1->have_option("l") ? cp1->option_value("l") : (1 << 24);
-		lc = cp1->have_option("c") ? cp1->option_value("c") : (3);
-		lp = cp1->have_option("p") ? cp1->option_value("p") : (0);
-		pb = cp1->have_option("b") ? cp1->option_value("b") : (2);
-		fb = cp1->have_option("f") ? cp1->option_value("f") : (32);
+		int level;
+		unsigned dictsize;
+		int lc;
+		int lp;
+		int pb;
+		int fb;
+		char *pend;
+		level = cp1->have_option("l") ? strtol(cp1->option_value("t").c_str(), &pend, 10) : (5);
+		dictsize = cp1->have_option("d") ? strtol(cp1->option_value("d").c_str(), &pend, 10) : (1 << 24);
+		lc = cp1->have_option("c") ? strtol(cp1->option_value("c").c_str(), &pend, 10) : (3);
+		lp = cp1->have_option("p") ? strtol(cp1->option_value("p").c_str(), &pend, 10) : (0);
+		pb = cp1->have_option("b") ? strtol(cp1->option_value("b").c_str(), &pend, 10) : (2);
+		fb = cp1->have_option("f") ? strtol(cp1->option_value("f").c_str(), &pend, 10) : (32);
+
+		ret = mycompress(infile, outfile, level, dictsize, lc, lp, pb, fb);
+	} else if(lcp.get_curparser() == cp2) {
+		ret = mydecompress(infile, outfile);
 	}
 
-	//if(lcp.get_curparser() == cp1) {
-
-	//	unsigned char *psrcRead = new unsigned char[filesize]; //原始文件数据
-	//	unsigned char *pDecomress = new unsigned char[filesize]; //存放解压缩数据
-	//	unsigned char *pLzma = new unsigned char[destLen]; //存放压缩数据
-
-	//	fread(psrcRead,sizeof(char),filesize,binfile);
-
-	//	unsigned char prop[5];
-	//	size_t sizeProp = 5;
-
-	//	if (SZ_OK != LzmaCompress(pLzma,&destLen,psrcRead,filesize,prop,&sizeProp,5,(1<<24),3,0,2,32,2)) {//出错了
-	//		printf("[%s:%s:%d]", __FILE__, __PRETTY_FUNCTION__, __LINE__);
-	//		printf("%s:%m\n", strerror(errno));
-	//		printf("压缩时出错！");
-	//		delete psrcRead;
-	//		delete pDecomress;
-	//		delete pLzma;
-	//		fclose(binfile);
-	//		return -1;
-	//	}
-	//}
-
-	//bool b_encrypt = opt.have_option("e");
-	//bool b_decrypt = opt.have_option("d");
-	//if(b_encrypt && b_decrypt) {
-	//	printf("%s:%s:%d\n", __FILE__, __PRETTY_FUNCTION__, __LINE__);
-	//	ret = -1;
-	//	return ret;
-	//}
-
-	//if(opt.have_option("i") && opt.have_option("o")) {
-	//	infile = opt.option_value("i");
-	//	outfile = opt.option_value("o");
-	//}
-
-	//printf("infile:%s, outfile:%s\n", infile.c_str(), outfile.c_str());
-
-	//if(b_encrypt) {
-	//	if(access(infile.c_str(), F_OK) != 0) {
-	//		printf("error: %s(errno: %d)\n",strerror(errno),errno);
-	//		ret = -1;
-	//		return ret;
-	//	}
-	//	data_encrypt de(infile.c_str(), outfile.c_str());
-	//	while(de.do_cache_file() != -1) {
-	//		de.cache_process();
-	//	}
-	//	remove(infile.c_str());
-	//}
-
-	//if(b_decrypt) {
-	//	if(access(outfile.c_str(), F_OK) != 0) {
-	//		printf("error: %s(errno: %d)\n",strerror(errno),errno);
-	//		ret = -1;
-	//		return ret;
-	//	}
-	//	data_decrypt dd(outfile.c_str(), infile.c_str());
-	//	while(dd.do_cache_file() != -1) {
-	//		dd.cache_process();
-	//	}
-	//	remove(outfile.c_str());
-	//}
-
-	//log log;
-	//log.set_logfile("test.txt");
-	//std::cout << "This is written to the file" << std::endl;
-	//log.reset_logfile();
 
 	return ret;
 }
