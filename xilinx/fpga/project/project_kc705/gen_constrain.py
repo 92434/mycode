5520fbb13f4db602281189da041ca92ec8fe586b
xiaofei <fei.xiao@lumin-tech.com>
2015-10-15 18:09:27 +0800

    add kd705 default constrain

--- xilinx/fpga/project/project_kc705/gen_constrain.py
+++ xilinx/fpga/project/project_kc705/gen_constrain.py
@@ -533,6 +533,72 @@ def gen_hpc_lpc_pins_registor_map(resistor_kc705_pins_resistor_map):
 	return hpc_lpc_pins_registor_map
 
 
+def gen_default_contrain():
+	txt = """
+# Sys Clock Pins
+set_property PACKAGE_PIN AD11 [get_ports EXT_SYS_CLK_clk_n]
+set_property IOSTANDARD DIFF_SSTL15 [get_ports EXT_SYS_CLK_clk_n]
+
+set_property PACKAGE_PIN AD12 [get_ports EXT_SYS_CLK_clk_p]
+set_property IOSTANDARD DIFF_SSTL15 [get_ports EXT_SYS_CLK_clk_p]
+
+# Sys Reset Pins
+set_property PACKAGE_PIN AB7 [get_ports reset]
+set_property IOSTANDARD LVCMOS15 [get_ports reset]
+
+# PCIe Refclk Pins
+set_property PACKAGE_PIN U8 [get_ports EXT_PCIE_REFCLK_P]
+set_property PACKAGE_PIN U7 [get_ports EXT_PCIE_REFCLK_N]
+
+# PCIe TX RX Pins
+#set_property PACKAGE_PIN M6 [get_ports {EXT_PCIE_rxp[0]}]
+#set_property PACKAGE_PIN M5 [get_ports {EXT_PCIE_rxn[0]}]
+#set_property PACKAGE_PIN P6 [get_ports {EXT_PCIE_rxp[1]}]
+#set_property PACKAGE_PIN P5 [get_ports {EXT_PCIE_rxn[1]}]
+#set_property PACKAGE_PIN R4 [get_ports {EXT_PCIE_rxp[2]}]
+#set_property PACKAGE_PIN R3 [get_ports {EXT_PCIE_rxn[2]}]
+#set_property PACKAGE_PIN T6 [get_ports {EXT_PCIE_rxp[3]}]
+#set_property PACKAGE_PIN T5 [get_ports {EXT_PCIE_rxn[3]}]
+#set_property PACKAGE_PIN L4 [get_ports {EXT_PCIE_txp[0]}]
+#set_property PACKAGE_PIN L3 [get_ports {EXT_PCIE_txn[0]}]
+#set_property PACKAGE_PIN M2 [get_ports {EXT_PCIE_txp[1]}]
+#set_property PACKAGE_PIN M1 [get_ports {EXT_PCIE_txn[1]}]
+#set_property PACKAGE_PIN N4 [get_ports {EXT_PCIE_txp[2]}]
+#set_property PACKAGE_PIN N3 [get_ports {EXT_PCIE_txn[2]}]
+#set_property PACKAGE_PIN P2 [get_ports {EXT_PCIE_txp[3]}]
+#set_property PACKAGE_PIN P1 [get_ports {EXT_PCIE_txn[3]}]
+
+# LED Pins
+set_property PACKAGE_PIN AB8 [get_ports {EXT_LEDS[0]}]
+set_property IOSTANDARD LVCMOS15 [get_ports {EXT_LEDS[0]}]
+
+set_property PACKAGE_PIN AA8 [get_ports {EXT_LEDS[1]}]
+set_property IOSTANDARD LVCMOS15 [get_ports {EXT_LEDS[1]}]
+
+set_property PACKAGE_PIN AC9 [get_ports {EXT_LEDS[2]}]
+set_property IOSTANDARD LVCMOS15 [get_ports {EXT_LEDS[2]}]
+
+set_property PACKAGE_PIN AB9 [get_ports {EXT_LEDS[3]}]
+set_property IOSTANDARD LVCMOS15 [get_ports {EXT_LEDS[3]}]
+
+set_property PACKAGE_PIN AE26 [get_ports {EXT_LEDS[4]}]
+set_property IOSTANDARD LVCMOS18 [get_ports {EXT_LEDS[4]}]
+
+set_property PACKAGE_PIN G19 [get_ports {EXT_LEDS[5]}]
+set_property IOSTANDARD LVCMOS18 [get_ports {EXT_LEDS[5]}]
+
+set_property PACKAGE_PIN E18 [get_ports {EXT_LEDS[6]}]
+set_property IOSTANDARD LVCMOS18 [get_ports {EXT_LEDS[6]}]
+
+set_property PACKAGE_PIN F16 [get_ports {EXT_LEDS[7]}]
+set_property IOSTANDARD LVCMOS18 [get_ports {EXT_LEDS[7]}]
+	"""
+
+	print '#', '-' * 100
+	print '#', 'kc705 default constrain'
+	print '#', '-' * 100
+	print txt
+
 def gen_constrain(hpc_lpc_pins_registor_map, fmc_type, start):
 	pins_registor = hpc_lpc_pins_registor_map.get(fmc_type)
 	ports = gen_gpio_ports(gpio_groups)
@@ -561,9 +627,9 @@ def gen_constrain(hpc_lpc_pins_registor_map, fmc_type, start):
 
 		constrain.append((io, pin, gpio))
 
-	print '-' * 100
-	print 'generator constrain for %s' %(fmc_type)
-	print '-' * 100
+	print '#', '-' * 100
+	print '#', 'generator constrain for %s' %(fmc_type)
+	print '#', '-' * 100
 	constrain = sorted(constrain, key = lambda x:(constrain_key_gpio(x)))
 	for i in constrain:
 		io, pin, gpio = i
@@ -608,7 +674,10 @@ def gen_kc705_constrain():
 
 	resistor_kc705_pins_resistor_map = gen_resistor_kc705_pins_resistor_map(kc705_pins_resistor)
 	hpc_lpc_pins_registor_map = gen_hpc_lpc_pins_registor_map(resistor_kc705_pins_resistor_map)
-	
+
+	#输出固定约束
+	gen_default_contrain()
+
 	#输出fmc板上电阻与kc705的映射关系
 	hpc_constrain = gen_constrain(hpc_lpc_pins_registor_map, 'HPC', 0)
 	lpc_constrain = gen_constrain(hpc_lpc_pins_registor_map, 'LPC', len(hpc_constrain))
