ee2006fe3e9af97f3daa03185766ca7431da7be0
xiaofei <fei.xiao@lumin-tech.com>
2015-11-24 11:28:43 +0800

    update makefile test

---
+++ make_test/inc.mk
@@ -0,0 +1,10 @@
+include $(default_rules)
+
+define my-dir
+$(strip \
+  $(eval LOCAL_MODULE_MAKEFILE := $(lastword $(MAKEFILE_LIST))) \
+  $(info LOCAL_MODULE_MAKEFILE: $(LOCAL_MODULE_MAKEFILE)) \
+  $(patsubst %/,%,$(dir $(LOCAL_MODULE_MAKEFILE))))
+endef
+
+$(info my-dir: $(call my-dir))
