bb457d9d571a553a61a22c0f5b1564d76c6e9881
xiaofei <fei.xiao@lumin-tech.com>
2016-02-29 18:19:04 +0800

    update lcd fb driver:add a picture for test

--- xilinx/fpga/project/project_kc705/src/driver/eg9013f_nz_fb.h
+++ xilinx/fpga/project/project_kc705/src/driver/eg9013f_nz_fb.h
@@ -2,7 +2,7 @@
 #define _EG9013F_NZ_H
 #define EG9013F_NZ_WIDTH 640
 #define EG9013F_NZ_HEIGHT 480
-#define EG9013F_NZ_SIZE (EG9013F_NZ_WIDTH * EG9013F_NZ_HEIGHT)
+#define EG9013F_NZ_SIZE (EG9013F_NZ_WIDTH * EG9013F_NZ_HEIGHT / 8)
 int eg9013f_nz_fb_add(kc705_pci_dev_t *kc705_pci_dev);
 int eg9013f_nz_fb_remove(kc705_pci_dev_t *kc705_pci_dev);
 #endif//#ifndef _EG9013F_NZ_H
