836c476113a8db2476673e3617507f00cdeabc38
xiaofei <fei.xiao@lumin-tech.com>
2016-07-25 17:51:00 +0800

    update test for dir

--- python/package/p/p1/p1.py
+++ python/package/p/p1/p1.py
@@ -3,17 +3,17 @@ import traceback
 import sys
 print traceback.extract_stack()[-1][:-1]
 
-def p1_main(argv):
-	print traceback.extract_stack()[-1][:-1]
-	print argv
-
+def get_dict_type(d):
 	print '*' * 100
-	d = {}
 	dd = dir(d)
 	for i in dd:
 		j = getattr(d, i, None)
 		print "str:\033[31m$%s\033[0m\ntype:\033[31m$%s\033[0m\n" %(str(j), type(j))
 	print '*' * 100
+
+def p1_main(argv):
+	print traceback.extract_stack()[-1][:-1]
+	print argv
 
 if __name__ == "__main__":
 	py_main(sys.argv)
--- python/package/test_p.py
+++ python/package/test_p.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import traceback
-from p.p1.p1 import p1_main
+from p.p1.p1 import get_dict_type, p1_main
 
 if __name__ == "__main__":
 	print traceback.extract_stack()[-1][:-1]
 	print dir(p1_main)
-	p1_main(None)
+	d = {}
+	get_dict_type(d)
+	print dir()
