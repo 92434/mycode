a3a5ee24cc6b31d35b8102aea39546624e6c7f0a
xiaofei <fei.xiao@lumin-tech.com>
2016-07-25 17:40:54 +0800

    update code

--- python/package/p/p1/p1.py
+++ python/package/p/p1/p1.py
@@ -7,5 +7,13 @@ def p1_main(argv):
 	print traceback.extract_stack()[-1][:-1]
 	print argv
 
+	print '*' * 100
+	d = {}
+	dd = dir(d)
+	for i in dd:
+		j = getattr(d, i, None)
+		print "str:\033[31m$%s\033[0m\ntype:\033[31m$%s\033[0m\n" %(str(j), type(j))
+	print '*' * 100
+
 if __name__ == "__main__":
 	py_main(sys.argv)
--- python/package/test_p.py
+++ python/package/test_p.py
@@ -5,3 +5,4 @@ from p.p1.p1 import p1_main
 if __name__ == "__main__":
 	print traceback.extract_stack()[-1][:-1]
 	print dir(p1_main)
+	p1_main(None)
