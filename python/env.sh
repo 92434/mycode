38e4fe447c07bcdd6ca3e7fda3b9a96a7f3f9797
xiaofei <alibaba22001@163.com>
2016-07-28 01:14:54 +0800

    update env script

--- python/env.sh
+++ python/env.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
 
-export PYTHONPATH="$(pwd)/utils:$PYTHONPATH"
+pwd_="$(pwd)/utils"
+
+c=$(echo $PYTHONPATH | sed 's/ /&/g' | sed 's/:/ /g')
+#echo "c: $c"
+
+t=($c)
+#echo "\${#t[@]}: ${#t[@]}"
+
+item=
+path_exist=0
+for((i=0;i<${#t[@]};i+=1));do
+	item="${t[i]}"
+	item=$(echo $item | sed 's/&/ /g')
+	if [ "$item" = "$pwd_" ];then
+		path_exist=1;
+		break;
+	fi
+done
+
+if [ $path_exist -eq 1 ];then
+	echo match
+	:
+else
+	echo mismatch
+	export PYTHONPATH=${PYTHONPATH:+$PYTHONPATH:}"$pwd_"
+fi
