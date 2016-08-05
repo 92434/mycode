1eb2f279c8b74316c55332d7b53d64da5733a8bc
xiaofei <fei.xiao@lumin-tech.com>
2016-08-05 18:31:00 +0800

    update python app

--- python/app/decorator.py
+++ python/app/decorator.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 
-import traceback
+#import traceback
+from log import *
 
 def decorator(*args, **kwargs):
-	print traceback.extract_stack()[-1][:-1]
-	print 'args', args
-	print 'kwargs', kwargs
+	#print traceback.extract_stack()[-1][:-1]
+	txt = 'args:%s, kwargs:%s' %(str(args), str(kwargs))
+	logger.debug(txt)
 	return args[0]
 
 def decorator_proxy(*args, **kwargs):
-	print traceback.extract_stack()[-1][:-1]
-	print 'args', args
-	print 'kwargs', kwargs
+	#print traceback.extract_stack()[-1][:-1]
+	txt = 'args:%s, kwargs:%s' %(str(args), str(kwargs))
+	logger.debug(txt)
 	return args[0]
 
 @decorator(decorator_proxy)
 def test_decorator(*args):
-	print traceback.extract_stack()[-1][:-1]
-	print 'args', args
+	#print traceback.extract_stack()[-1][:-1]
+	txt = 'args:%s' %(str(args))
+	logger.debug(txt)
 
 
 if __name__ == '__main__':
