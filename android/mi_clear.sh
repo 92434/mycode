f1b4e6985d9d6d26f3dbb0780169b90ed41643bd
xiaofei <fei.xiao@lumin-tech.com>
2016-09-12 17:49:56 +0800

    add mi clear script

---
+++ android/mi_clear.sh
@@ -0,0 +1,292 @@
+#!/bin/sh
+
+#package:/system/priv-app/MiuiGallery.apk=com.miui.gallery
+#package:/data/app/com.explusalpha.NesEmu-1.apk=com.explusalpha.NesEmu
+#package:/system/priv-app/DefaultContainerService.apk=com.android.defcontainer
+#package:/data/app/com.tencent.mm-1.apk=com.tencent.mm
+#package:/system/app/QuickSearchBox.apk=com.android.quicksearchbox
+#package:/data/app/com.icbc-1.apk=com.icbc
+#package:/system/priv-app/Contacts.apk=com.android.contacts
+#package:/system/app/LocationEM.apk=com.mediatek.lbs.em
+#package:/system/priv-app/TeleService.apk=com.android.phone
+#package:/data/app/com.tencent.lightalk-1.apk=com.tencent.lightalk
+#package:/system/priv-app/Music.apk=com.miui.player
+#package:/system/priv-app/MiuiVoip.apk=com.miui.voip
+#package:/system/app/HTMLViewer.apk=com.android.htmlviewer
+#package:/data/app/com.xiaomi.mitunes-1.apk=com.xiaomi.mitunes
+#package:/system/app/MiuiScanner.apk=com.xiaomi.scanner
+#package:/system/app/GuardProvider.apk=com.miui.guardprovider
+#package:/system/app/Bluetooth.apk=com.android.bluetooth
+#package:/system/priv-app/CalendarProvider.apk=com.android.providers.calendar
+#package:/system/priv-app/Mipub.apk=com.miui.mipub
+#package:/system/priv-app/YellowPage.apk=com.miui.yellowpage
+#package:/system/priv-app/Calendar.apk=com.android.calendar
+#package:/data/app/com.MobileTicket-1.apk=com.MobileTicket
+#package:/system/app/MiLinkService.apk=com.milink.service
+#package:/system/priv-app/Browser.apk=com.android.browser
+#package:/system/app/MTKLogger.apk=com.mediatek.mtklogger
+#package:/system/priv-app/Backup.apk=com.miui.backup
+#package:/system/app/AppIndexProvider.apk=com.xiaomi.providers.appindex
+#package:/system/app/AntiSpam.apk=com.miui.antispam
+#package:/system/app/CloudService.apk=com.miui.cloudservice
+#package:/system/app/Notes.apk=com.miui.notes
+#package:/system/priv-app/OneTimeInitializer.apk=com.android.onetimeinitializer
+#package:/system/app/SecurityCoreAdd.apk=com.miui.securitycore
+#package:/system/priv-app/DownloadProviderUi.apk=com.android.providers.downloads.ui
+#package:/system/app/SmsExtra.apk=com.miui.smsextra
+#package:/system/app/DocumentsUI.apk=com.android.documentsui
+#package:/system/priv-app/SharedStorageBackup.apk=com.android.sharedstoragebackup
+#package:/system/priv-app/WeatherProvider.apk=com.miui.providers.weather
+#package:/system/priv-app/VpnDialogs.apk=com.android.vpndialogs
+#package:/system/priv-app/Mms.apk=com.android.mms
+#package:/system/app/Provision.apk=com.android.provision
+#package:/system/app/VoiceAssist.apk=com.miui.voiceassist
+#package:/system/priv-app/MediaProvider.apk=com.android.providers.media
+#package:/system/priv-app/FindDevice.apk=com.xiaomi.finddevice
+#package:/system/app/KingSoftCleaner.apk=com.cleanmaster.sdk
+#package:/system/app/CertInstaller.apk=com.android.certinstaller
+#package:/data/app/com.example.android.apis-1.apk=com.example.android.apis
+#package:/system/app/ThemeManager.apk=com.android.thememanager
+#package:/system/app/MiuiCompass.apk=com.miui.compass
+#package:/system/app/MtkBt.apk=com.mediatek.bluetooth
+#package:/system/framework/framework-ext-res.apk=com.miui.rom
+#package:/system/app/PhotoTable.apk=com.android.dreams.phototable
+#package:/data/app/com.baidu.input_miv6-1.apk=com.baidu.input_miv6
+#package:/system/app/Updater.apk=com.android.updater
+#package:/system/priv-app/MiDrop.apk=com.xiaomi.midrop
+#package:/system/app/FWUpgradeProvider.apk=com.fw.upgrade.sysoper
+#package:/system/priv-app/Settings.apk=com.android.settings
+#package:/system/app/FileExplorer.apk=com.android.fileexplorer
+#package:/system/priv-app/AuthManager.apk=com.lbe.security.miui
+#package:/system/app/mab.apk=com.xiaomi.ab
+#package:/data/app/com.sinovatech.unicom.ui-1.apk=com.sinovatech.unicom.ui
+#package:/system/app/FWUpgrade.apk=com.fw.upgrade
+#package:/system/app/MiuiBluetooth.apk=com.xiaomi.bluetooth
+#package:/data/app/com.eg.android.AlipayGphone-1.apk=com.eg.android.AlipayGphone
+#package:/system/app/DrmProvider.apk=com.android.providers.drm
+#package:/system/app/VisualizationWallpapers.apk=com.android.musicvis
+#package:/system/app/NetworkAssistant2.apk=com.miui.networkassistant
+#package:/system/app/LiveWallpapersPicker.apk=com.android.wallpaper.livepicker
+#package:/system/app/SecurityAdd.apk=com.miui.securityadd
+#package:/system/app/PackageInstaller.apk=com.android.packageinstaller
+#package:/data/app/com.xiaomi.gamecenter-1.apk=com.xiaomi.gamecenter
+#package:/system/app/TelephonyProvider.apk=com.android.providers.telephony
+#package:/system/app/CellConnService.apk=com.mediatek.CellConnService
+#package:/system/priv-app/MiuiHome.apk=com.miui.home
+#package:/system/app/PicoTts.apk=com.svox.pico
+#package:/system/app/NoiseField.apk=com.android.noisefield
+#package:/system/app/Email.apk=com.android.email
+#package:/system/app/PowerKeeper.apk=com.miui.powerkeeper
+#package:/system/app/wt_version_query.apk=com.wt.version_query
+#package:/data/app/com.baidu.BaiduMap-1.apk=com.baidu.BaiduMap
+#package:/system/app/Weather3DWidget.apk=com.mediatek.weather3dwidget
+#package:/system/priv-app/WallpaperCropper.apk=com.android.wallpapercropper
+#package:/system/priv-app/FusedLocation.apk=com.android.location.fused
+#package:/system/priv-app/BackupRestoreConfirmation.apk=com.android.backupconfirm
+#package:/system/app/MagicSmokeWallpapers.apk=com.android.magicsmoke
+#package:/system/priv-app/SettingsProvider.apk=com.android.providers.settings
+#package:/system/app/miui.apk=com.miui.core
+#package:/system/priv-app/DownloadProvider.apk=com.android.providers.downloads
+#package:/system/app/SystemAdSolution.apk=com.miui.systemAdSolution
+#package:/system/app/BrowserProviderProxy.apk=com.android.browser.provider
+#package:/system/app/MusicFX.apk=com.android.musicfx
+#package:/system/app/PhaseBeam.apk=com.android.phasebeam
+#package:/system/app/SoundRecorder.apk=com.android.soundrecorder
+#package:/data/app/klobugreport.apk=com.miui.klo.bugreport
+#package:/system/app/Metok.apk=com.xiaomi.metok
+#package:/system/priv-app/CleanMaster.apk=com.miui.cleanmaster
+#package:/system/app/Whetstone.apk=com.miui.whetstone
+#package:/system/app/FactoryMode.apk=com.mediatek.factorymode
+#package:/system/priv-app/ProxyHandler.apk=com.android.proxyhandler
+#package:/data/app/com.qihoo.browser-1.apk=com.qihoo.browser
+#package:/data/app/com.mxtech.videoplayer.ad-1.apk=com.mxtech.videoplayer.ad
+#package:/system/app/XiaomiAccount.apk=com.xiaomi.account
+#package:/data/app/com.xiaomi.pass-1.apk=com.xiaomi.pass
+#package:/system/app/Mipay.apk=com.mipay.wallet
+#package:/system/priv-app/InputDevices.apk=com.android.inputdevices
+#package:/system/app/HoloSpiralWallpaper.apk=com.android.wallpaper.holospiral
+#package:/system/app/BugReport.apk=com.miui.bugreport
+#package:/system/app/FM.apk=com.miui.fm
+#package:/system/app/TouchAssistant.apk=com.miui.touchassistant
+#package:/system/app/MiWallpaper.apk=com.miui.miwallpaper
+#package:/system/app/Stk1.apk=com.android.stk
+#package:/data/app/com.xiaofei.wifiadb-1.apk=com.xiaofei.wifiadb
+#package:/system/app/UserDictionaryProvider.apk=com.android.providers.userdictionary
+#package:/system/app/SystemBaseFunctions.apk=com.miui.sysbase
+#package:/data/app/com.tencent.mobileqq-1.apk=com.tencent.mobileqq
+#package:/system/app/PacProcessor.apk=com.android.pacprocessor
+#package:/system/app/Galaxy4.apk=com.android.galaxy4
+#package:/data/app/com.miui.weather2-1.apk=com.miui.weather2
+#package:/system/app/MtkWeatherProvider.apk=com.mediatek.weather
+#package:/data/app/com.mfashiongallery.emag-2.apk=com.mfashiongallery.emag
+#package:/system/app/PrintSpooler.apk=com.android.printspooler
+#package:/system/app/MiuiSuperMarket.apk=com.xiaomi.market
+#package:/system/framework/mediatek-res.apk=com.mediatek
+#package:/system/framework/framework-res.apk=android
+#package:/system/priv-app/ContactsProvider.apk=com.android.providers.contacts
+#package:/system/priv-app/SecurityCenter.apk=com.miui.securitycenter
+#package:/system/app/BaiduNetworkLocation.apk=com.baidu.map.location
+#package:/mnt/asec/com.handsgo.jiakao.android-1/pkg.apk=com.handsgo.jiakao.android
+#package:/system/priv-app/ExternalStorageProvider.apk=com.android.externalstorage
+#package:/system/app/ApplicationsProvider.apk=com.android.providers.applications
+#package:/system/app/Calculator.apk=com.miui.calculator
+#package:/system/app/BasicDreams.apk=com.android.dreams.basic
+#package:/system/app/InCallUI.apk=com.android.incallui
+#package:/system/app/TranslationService.apk=com.miui.translationservice
+#package:/data/app/com.speedsoftware.rootexplorer-2.apk=com.speedsoftware.rootexplorer
+#package:/system/priv-app/MiuiSystemUI.apk=com.android.systemui
+#package:/data/app/com.kingpoint.gmcchh-1.apk=com.kingpoint.gmcchh
+#package:/system/app/EngineerModeSim.apk=com.android.simmelock
+#package:/system/app/KeyChain.apk=com.android.keychain
+#package:/system/app/WAPPushManager.apk=com.android.smspush
+#package:/system/app/LiveWallpapers.apk=com.android.wallpaper
+#package:/system/app/MtkWeatherWidget.apk=com.mediatek.appwidget.weather
+#package:/data/app/com.sohu.inputmethod.sogou.xiaomi-1.apk=com.sohu.inputmethod.sogou.xiaomi
+#package:/system/priv-app/MiuiCamera.apk=com.android.camera
+#package:/system/app/StepsProvider.apk=com.miui.stepsprovider
+#package:/system/app/EngineerMode.apk=com.mediatek.engineermode
+#package:/system/priv-app/CloudBackup.apk=com.miui.cloudbackup
+#package:/system/app/DeskClock.apk=com.android.deskclock
+#package:/system/priv-app/MiGameCenterSDKService.apk=com.xiaomi.gamecenter.sdk.service
+#package:/data/app/com.kugou.android-1.apk=com.kugou.android
+#package:/system/app/AnalyticsCore.apk=com.miui.analytics
+#package:/system/priv-app/MiuiKeyguard.apk=com.android.keyguard
+#package:/system/app/miuisystem.apk=com.miui.system
+#package:/system/app/KSICibaEngine.apk=com.miui.translation.kingsoft
+#package:/system/app/XiaomiVip.apk=com.xiaomi.vip
+#package:/system/priv-app/Shell.apk=com.android.shell
+#package:/system/app/XiaomiServiceFramework.apk=com.xiaomi.xmsf
+#package:/data/app/com.coomix.app.bus-1.apk=com.coomix.app.bus
+#package:/system/app/PaymentService.apk=com.xiaomi.payment
+#package:/data/app/com.miui.video-1.apk=com.miui.video
+#package:/system/app/YGPS.apk=com.mediatek.ygps
+
+#usage: pm list packages [-f] [-d] [-e] [-s] [-3] [-i] [-u] [--user USER_ID] [FILTER]
+#       pm list permission-groups
+#       pm list permissions [-g] [-f] [-d] [-u] [GROUP]
+#       pm list instrumentation [-f] [TARGET-PACKAGE]
+#       pm list features
+#       pm list libraries
+#       pm list users
+#       pm path PACKAGE
+#       pm dump PACKAGE
+#       pm install [-l] [-r] [-t] [-i INSTALLER_PACKAGE_NAME] [-s] [-f]
+#                  [--algo <algorithm name> --key <key-in-hex> --iv <IV-in-hex>]
+#                  [--originating-uri <URI>] [--referrer <URI>] PATH
+#       pm uninstall [-k] PACKAGE
+#       pm clear [--user USER_ID] PACKAGE
+#       pm enable [--user USER_ID] PACKAGE_OR_COMPONENT
+#       pm disable [--user USER_ID] PACKAGE_OR_COMPONENT
+#       pm disable-user [--user USER_ID] PACKAGE_OR_COMPONENT
+#       pm disable-until-used [--user USER_ID] PACKAGE_OR_COMPONENT
+#       pm block [--user USER_ID] PACKAGE_OR_COMPONENT
+#       pm unblock [--user USER_ID] PACKAGE_OR_COMPONENT
+#       pm grant PACKAGE PERMISSION
+#       pm revoke PACKAGE PERMISSION
+#       pm set-install-location [0/auto] [1/internal] [2/external]
+#       pm get-install-location
+#       pm set-permission-enforced PERMISSION [true|false]
+#       pm trim-caches DESIRED_FREE_SPACE
+#       pm create-user USER_NAME
+#       pm remove-user USER_ID
+#       pm get-max-users
+#
+#pm list packages: prints all packages, optionally only
+#  those whose package name contains the text in FILTER.  Options:
+#    -f: see their associated file.
+#    -d: filter to only show disbled packages.
+#    -e: filter to only show enabled packages.
+#    -s: filter to only show system packages.
+#    -3: filter to only show third party packages.
+#    -i: see the installer for the packages.
+#    -u: also include uninstalled packages.
+#
+#pm list permission-groups: prints all known permission groups.
+#
+#pm list permissions: prints all known permissions, optionally only
+#  those in GROUP.  Options:
+#    -g: organize by group.
+#    -f: print all information.
+#    -s: short summary.
+#    -d: only list dangerous permissions.
+#    -u: list only the permissions users will see.
+#
+#pm list instrumentation: use to list all test packages; optionally
+#  supply <TARGET-PACKAGE> to list the test packages for a particular
+#  application.  Options:
+#    -f: list the .apk file for the test package.
+#
+#pm list features: prints all features of the system.
+#
+#pm list users: prints all users on the system.
+#
+#pm path: print the path to the .apk of the given PACKAGE.
+#
+#pm dump: print system state associated w ith the given PACKAGE.
+#
+#pm install: installs a package to the system.  Options:
+#    -l: install the package with FORWARD_LOCK.
+#    -r: reinstall an exisiting app, keeping its data.
+#    -t: allow test .apks to be installed.
+#    -i: specify the installer package name.
+#    -s: install package on sdcard.
+#    -f: install package on internal flash.
+#    -d: allow version code downgrade.
+#
+#pm uninstall: removes a package from the system. Options:
+#    -k: keep the data and cache directories around after package removal.
+#
+#pm clear: deletes all data associated with a package.
+#
+#pm enable, disable, disable-user, disable-until-used: these commands
+#  change the enabled state of a given package or component (written
+#  as "package/class").
+#
+#pm grant, revoke: these commands either grant or revoke permissions
+#  to applications.  Only optional permissions the application has
+#  declared can be granted or revoked.
+#
+#pm get-install-location: returns the current install location.
+#    0 [auto]: Let system decide the best location
+#    1 [internal]: Install on internal device storage
+#    2 [external]: Install on external media
+#
+#pm set-install-location: changes the default install location.
+#  NOTE: this is only intended for debugging; using this can cause
+#  applications to break and other undersireable behavior.
+#    0 [auto]: Let system decide the best location
+#    1 [internal]: Install on internal device storage
+#    2 [external]: Install on external media
+#
+#pm trim-caches: trim cache files to reach the given free space.
+#
+#pm create-user: create a new user with the given USER_NAME,
+#  printing the new user identifier of the user.
+#
+#pm remove-user: remove the user with the given USER_IDENTIFIER,
+#  deleting all data associated with that user
+
+function main() {
+	local ip=
+
+	ip="$1"
+	if [ -z "$ip" ]; then
+		echo "error!!!";
+		return;
+	fi
+
+	adb connect $*
+	adb root
+
+	#clear
+	#adb shell pm clear com.sohu.inputmethod.sogou.xiaomi
+	adb shell pm clear com.xiaomi.gamecenter
+	adb shell pm clear com.xiaomi.gamecenter.sdk.service
+	adb shell pm clear com.android.browser
+	adb shell pm clear com.kugou.android
+
+	#uninstall
+	#adb shell pm uninstall com.sohu.inputmethod.sogou.xiaomi
+}
+
+main $@
