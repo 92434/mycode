36bc8f09bcb6710e3374d401f20506798e63d31b
xiaofei <fei.xiao@lumin-tech.com>
2016-10-20 12:22:31 +0800

    add debug header

---
+++ c/utils/xiaofei_debug.h
@@ -0,0 +1,21 @@
+#ifndef _XIAOFEI_DEBUG_H
+#define _XIAOFEI_DEBUG_H
+#include <stdio.h>
+
+#define mydebug(format, ...) printf("[%s:%s:%d]:"format, __FILE__, __PRETTY_FUNCTION__, __LINE__, ## __VA_ARGS__)
+#define myprintf(format, ...) printf(format, ## __VA_ARGS__)
+#define myprintf_once(condition, format, ...) ({ \
+	static int state = 0;\
+	int myprintf_once_ret = 0; \
+	\
+	if(condition) { \
+		if(state == 0) { \
+			myprintf_once_ret = printf(format, ## __VA_ARGS__); \
+			state = 1; \
+		} else {} \
+	} else { \
+		state = 0; \
+	} \
+	myprintf_once_ret; \
+})
+#endif //#ifndef _XIAOFEI_DEBUG_H
