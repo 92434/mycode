25d638e30720fdc68e4e09138ded729fac7df98b
xiaofei <alibaba22001@163.com>
2016-11-09 23:55:06 +0800

    udpate python script

--- python/python_cscope_path.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-export tags_src_dir=$(python -c "import sys;print('%s' %(':'.join(sys.path)))")
--- sh/home/xiaofei/tags_env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-export tags_src_dir="/usr/lib/pymodules/python2.7:/usr/lib/python2.7:/usr/local/lib/python2.7"
