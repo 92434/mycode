c6249e58cd6590a3021a5c3d764a8f3f29303a58
xiaofei <alibaba22001@163.com>
2017-01-09 18:51:51 +0800

    update app for arm

--- c/kernel/app/getevent.c
+++ c/kernel/app/getevent.c
@@ -38,6 +38,7 @@ static const char *get_label(const struct label *labels, int value)
 	while(labels->name && value != labels->value) {
 		labels++;
 	}
+
 	return labels->name;
 }
 
@@ -51,25 +52,34 @@ static int print_input_props(int fd)
 
 	printf("  input props:\n");
 	res = ioctl(fd, EVIOCGPROP(sizeof(bits)), bits);
+
 	if(res < 0) {
 		printf("    <not available\n");
 		return 1;
 	}
+
 	count = 0;
+
 	for(i = 0; i < res; i++) {
 		for(j = 0; j < 8; j++) {
 			if (bits[i] & 1 << j) {
 				bit_label = get_label(input_prop_labels, i * 8 + j);
-				if(bit_label)
+
+				if(bit_label) {
 					printf("    %s\n", bit_label);
-				else
+				} else {
 					printf("    %04x\n", i * 8 + j);
+				}
+
 				count++;
 			}
 		}
 	}
-	if (!count)
+
+	if (!count) {
 		printf("    <none>\n");
+	}
+
 	return 0;
 }
 
@@ -77,27 +87,35 @@ static int print_possible_events(int fd, int print_flags)
 {
 	uint8_t *bits = NULL;
 	ssize_t bits_size = 0;
-	const char* label;
+	const char *label;
 	int i, j, k;
 	int res, res2;
-	struct label* bit_labels;
+	struct label *bit_labels;
 	const char *bit_label;
 
 	printf("  events:\n");
+
 	for(i = EV_KEY; i <= EV_MAX; i++) { // skip EV_SYN since we cannot query its available codes
 		int count = 0;
+
 		while(1) {
 			res = ioctl(fd, EVIOCGBIT(i, bits_size), bits);
-			if(res < bits_size)
+
+			if(res < bits_size) {
 				break;
+			}
+
 			bits_size = res + 16;
 			bits = realloc(bits, bits_size * 2);
+
 			if(bits == NULL) {
 				fprintf(stderr, "failed to allocate buffer of size %d\n", (int)bits_size);
 				return 1;
 			}
 		}
+
 		res2 = 0;
+
 		switch(i) {
 			case EV_KEY:
 				res2 = ioctl(fd, EVIOCGKEY(res), bits + bits_size);
@@ -152,41 +170,55 @@ static int print_possible_events(int fd, int print_flags)
 				label = "???";
 				bit_labels = NULL;
 		}
+
 		for(j = 0; j < res; j++) {
 			for(k = 0; k < 8; k++)
 				if(bits[j] & 1 << k) {
 					char down;
-					if(j < res2 && (bits[j + bits_size] & 1 << k))
+
+					if(j < res2 && (bits[j + bits_size] & 1 << k)) {
 						down = '*';
-					else
+					} else {
 						down = ' ';
-					if(count == 0)
+					}
+
+					if(count == 0) {
 						printf("    %s (%04x):", label, i);
-					else if((count & (print_flags & PRINT_LABELS ? 0x3 : 0x7)) == 0 || i == EV_ABS)
+					} else if((count & (print_flags & PRINT_LABELS ? 0x3 : 0x7)) == 0 || i == EV_ABS) {
 						printf("\n               ");
+					}
+
 					if(bit_labels && (print_flags & PRINT_LABELS)) {
 						bit_label = get_label(bit_labels, j * 8 + k);
-						if(bit_label)
+
+						if(bit_label) {
 							printf(" %.20s%c%*s", bit_label, down, 20 - strlen(bit_label), "");
-						else
+						} else {
 							printf(" %04x%c                ", j * 8 + k, down);
+						}
 					} else {
 						printf(" %04x%c", j * 8 + k, down);
 					}
+
 					if(i == EV_ABS) {
 						struct input_absinfo abs;
+
 						if(ioctl(fd, EVIOCGABS(j * 8 + k), &abs) == 0) {
 							printf(" : value %d, min %d, max %d, fuzz %d, flat %d, resolution %d",
-									abs.value, abs.minimum, abs.maximum, abs.fuzz, abs.flat,
-									abs.resolution);
+								   abs.value, abs.minimum, abs.maximum, abs.fuzz, abs.flat,
+								   abs.resolution);
 						}
 					}
+
 					count++;
 				}
 		}
-		if(count)
+
+		if(count) {
 			printf("\n");
+		}
 	}
+
 	free(bits);
 	return 0;
 }
@@ -213,10 +245,12 @@ static void print_event(int type, int code, int value, int print_flags)
 				break;
 			case EV_ABS:
 				code_label = get_label(abs_labels, code);
+
 				switch(code) {
 					case ABS_MT_TOOL_TYPE:
 						value_label = get_label(mt_tool_labels, value);
 				}
+
 				break;
 			case EV_MSC:
 				code_label = get_label(msc_labels, code);
@@ -241,18 +275,23 @@ static void print_event(int type, int code, int value, int print_flags)
 				break;
 		}
 
-		if (type_label)
+		if (type_label) {
 			printf("%-12.12s", type_label);
-		else
+		} else {
 			printf("%04x        ", type);
-		if (code_label)
+		}
+
+		if (code_label) {
 			printf(" %-20.20s", code_label);
-		else
+		} else {
 			printf(" %04x                ", code);
-		if (value_label)
+		}
+
+		if (value_label) {
 			printf(" %-20.20s", value_label);
-		else
+		} else {
 			printf(" %08x            ", value);
+		}
 	} else {
 		printf("%04x %04x %08x", type, code, value);
 	}
@@ -271,24 +310,31 @@ static void print_hid_descriptor(int bus, int vendor, int product)
 	snprintf(prefix, sizeof(prefix), "%04X:%04X:%04X.", bus, vendor, product);
 
 	dir = opendir(dirname);
-	if(dir == NULL)
+
+	if(dir == NULL) {
 		return;
+	}
+
 	while((de = readdir(dir))) {
 		if (strstr(de->d_name, prefix) == de->d_name) {
 			snprintf(filename, sizeof(filename), "%s/%s/rdesc", dirname, de->d_name);
 
 			file = fopen(filename, "r");
+
 			if (file) {
 				printf("  HID descriptor: %s\n\n", de->d_name);
+
 				while (fgets(line, sizeof(line), file)) {
 					fputs("    ", stdout);
 					fputs(line, stdout);
 				}
+
 				fclose(file);
 				puts("");
 			}
 		}
 	}
+
 	closedir(dir);
 }
 
@@ -304,67 +350,89 @@ static int open_device(const char *device, int print_flags)
 	struct input_id id;
 
 	fd = open(device, O_RDWR);
+
 	if(fd < 0) {
-		if(print_flags & PRINT_DEVICE_ERRORS)
+		if(print_flags & PRINT_DEVICE_ERRORS) {
 			fprintf(stderr, "could not open %s, %s\n", device, strerror(errno));
+		}
+
 		return -1;
 	}
 
 	if(ioctl(fd, EVIOCGVERSION, &version)) {
-		if(print_flags & PRINT_DEVICE_ERRORS)
+		if(print_flags & PRINT_DEVICE_ERRORS) {
 			fprintf(stderr, "could not get driver version for %s, %s\n", device, strerror(errno));
+		}
+
 		return -1;
 	}
+
 	if(ioctl(fd, EVIOCGID, &id)) {
-		if(print_flags & PRINT_DEVICE_ERRORS)
+		if(print_flags & PRINT_DEVICE_ERRORS) {
 			fprintf(stderr, "could not get driver id for %s, %s\n", device, strerror(errno));
+		}
+
 		return -1;
 	}
+
 	name[sizeof(name) - 1] = '\0';
 	location[sizeof(location) - 1] = '\0';
 	idstr[sizeof(idstr) - 1] = '\0';
+
 	if(ioctl(fd, EVIOCGNAME(sizeof(name) - 1), &name) < 1) {
 		//fprintf(stderr, "could not get device name for %s, %s\n", device, strerror(errno));
 		name[0] = '\0';
 	}
+
 	if(ioctl(fd, EVIOCGPHYS(sizeof(location) - 1), &location) < 1) {
 		//fprintf(stderr, "could not get location for %s, %s\n", device, strerror(errno));
 		location[0] = '\0';
 	}
+
 	if(ioctl(fd, EVIOCGUNIQ(sizeof(idstr) - 1), &idstr) < 1) {
 		//fprintf(stderr, "could not get idstring for %s, %s\n", device, strerror(errno));
 		idstr[0] = '\0';
 	}
 
 	new_ufds = realloc(ufds, sizeof(ufds[0]) * (nfds + 1));
+
 	if(new_ufds == NULL) {
 		fprintf(stderr, "out of memory\n");
 		return -1;
 	}
+
 	ufds = new_ufds;
 	new_device_names = realloc(device_names, sizeof(device_names[0]) * (nfds + 1));
+
 	if(new_device_names == NULL) {
 		fprintf(stderr, "out of memory\n");
 		return -1;
 	}
+
 	device_names = new_device_names;
 
-	if(print_flags & PRINT_DEVICE)
+	if(print_flags & PRINT_DEVICE) {
 		printf("add device %d: %s\n", nfds, device);
+	}
+
 	if(print_flags & PRINT_DEVICE_INFO)
 		printf("  bus:      %04x\n"
-				"  vendor    %04x\n"
-				"  product   %04x\n"
-				"  version   %04x\n",
-				id.bustype, id.vendor, id.product, id.version);
-	if(print_flags & PRINT_DEVICE_NAME)
+			   "  vendor    %04x\n"
+			   "  product   %04x\n"
+			   "  version   %04x\n",
+			   id.bustype, id.vendor, id.product, id.version);
+
+	if(print_flags & PRINT_DEVICE_NAME) {
 		printf("  name:     \"%s\"\n", name);
+	}
+
 	if(print_flags & PRINT_DEVICE_INFO)
 		printf("  location: \"%s\"\n"
-				"  id:       \"%s\"\n", location, idstr);
+			   "  id:       \"%s\"\n", location, idstr);
+
 	if(print_flags & PRINT_VERSION)
 		printf("  version:  %d.%d.%d\n",
-				version >> 16, (version >> 8) & 0xff, version & 0xff);
+			   version >> 16, (version >> 8) & 0xff, version & 0xff);
 
 	if(print_flags & PRINT_POSSIBLE_EVENTS) {
 		print_possible_events(fd, print_flags);
@@ -373,6 +441,7 @@ static int open_device(const char *device, int print_flags)
 	if(print_flags & PRINT_INPUT_PROPS) {
 		print_input_props(fd);
 	}
+
 	if(print_flags & PRINT_HID_DESCRIPTOR) {
 		print_hid_descriptor(id.bustype, id.vendor, id.product);
 	}
@@ -388,11 +457,15 @@ static int open_device(const char *device, int print_flags)
 int close_device(const char *device, int print_flags)
 {
 	int i;
+
 	for(i = 1; i < nfds; i++) {
 		if(strcmp(device_names[i], device) == 0) {
 			int count = nfds - i - 1;
-			if(print_flags & PRINT_DEVICE)
+
+			if(print_flags & PRINT_DEVICE) {
 				printf("remove device %d: %s\n", i, device);
+			}
+
 			free(device_names[i]);
 			memmove(device_names + i, device_names + i + 1, sizeof(device_names[0]) * count);
 			memmove(ufds + i, ufds + i + 1, sizeof(ufds[0]) * count);
@@ -400,8 +473,11 @@ int close_device(const char *device, int print_flags)
 			return 0;
 		}
 	}
-	if(print_flags & PRINT_DEVICE_ERRORS)
+
+	if(print_flags & PRINT_DEVICE_ERRORS) {
 		fprintf(stderr, "remote device: %s not found\n", device);
+	}
+
 	return -1;
 }
 
@@ -416,12 +492,16 @@ static int read_notify(const char *dirname, int nfd, int print_flags)
 	struct inotify_event *event;
 
 	res = read(nfd, event_buf, sizeof(event_buf));
+
 	if(res < (int)sizeof(*event)) {
-		if(errno == EINTR)
+		if(errno == EINTR) {
 			return 0;
+		}
+
 		fprintf(stderr, "could not get event, %s\n", strerror(errno));
 		return 1;
 	}
+
 	//printf("got %d bytes of event information\n", res);
 
 	strcpy(devname, dirname);
@@ -430,20 +510,23 @@ static int read_notify(const char *dirname, int nfd, int print_flags)
 
 	while(res >= (int)sizeof(*event)) {
 		event = (struct inotify_event *)(event_buf + event_pos);
+
 		//printf("%d: %08x \"%s\"\n", event->wd, event->mask, event->len ? event->name : "");
 		if(event->len) {
 			strcpy(filename, event->name);
+
 			if(event->mask & IN_CREATE) {
 				open_device(devname, print_flags);
-			}
-			else {
+			} else {
 				close_device(devname, print_flags);
 			}
 		}
+
 		event_size = sizeof(*event) + event->len;
 		res -= event_size;
 		event_pos += event_size;
 	}
+
 	return 0;
 }
 
@@ -454,23 +537,30 @@ static int scan_dir(const char *dirname, int print_flags)
 	DIR *dir;
 	struct dirent *de;
 	dir = opendir(dirname);
-	if(dir == NULL)
+
+	if(dir == NULL) {
 		return -1;
+	}
+
 	strcpy(devname, dirname);
 	filename = devname + strlen(devname);
 	*filename++ = '/';
+
 	while((de = readdir(dir))) {
 		if(
-				de->d_name[0] == '.'
-				&& (
-					de->d_name[1] == '\0' 
-					|| (de->d_name[1] == '.' && de->d_name[2] == '\0')
-				   )
-		  )
+			de->d_name[0] == '.'
+			&& (
+				de->d_name[1] == '\0'
+				|| (de->d_name[1] == '.' && de->d_name[2] == '\0')
+			)
+		) {
 			continue;
+		}
+
 		strcpy(filename, de->d_name);
 		open_device(devname, print_flags);
 	}
+
 	closedir(dir);
 	return 0;
 }
@@ -514,10 +604,14 @@ int main(int argc, char *argv[])
 	const char *device_path = "/dev/input";
 
 	opterr = 0;
+
 	do {
 		c = getopt(argc, argv, "tns:Sv::dpilqc:rh");
-		if (c == EOF)
+
+		if (c == EOF) {
 			break;
+		}
+
 		switch (c) {
 			case 't':
 				get_time = 1;
@@ -527,19 +621,28 @@ int main(int argc, char *argv[])
 				break;
 			case 's':
 				get_switch = strtoul(optarg, NULL, 0);
-				if(dont_block == -1)
+
+				if(dont_block == -1) {
 					dont_block = 1;
+				}
+
 				break;
 			case 'S':
 				get_switch = ~0;
-				if(dont_block == -1)
+
+				if(dont_block == -1) {
 					dont_block = 1;
+				}
+
 				break;
 			case 'v':
-				if(optarg)
+
+				if(optarg) {
 					print_flags |= strtoul(optarg, NULL, 0);
-				else
+				} else {
 					print_flags |= PRINT_DEVICE | PRINT_DEVICE_NAME | PRINT_DEVICE_INFO | PRINT_VERSION;
+				}
+
 				print_flags_set = 1;
 				break;
 			case 'd':
@@ -547,16 +650,22 @@ int main(int argc, char *argv[])
 				break;
 			case 'p':
 				print_flags |= PRINT_DEVICE_ERRORS | PRINT_DEVICE
-					| PRINT_DEVICE_NAME | PRINT_POSSIBLE_EVENTS | PRINT_INPUT_PROPS;
+							   | PRINT_DEVICE_NAME | PRINT_POSSIBLE_EVENTS | PRINT_INPUT_PROPS;
 				print_flags_set = 1;
-				if(dont_block == -1)
+
+				if(dont_block == -1) {
 					dont_block = 1;
+				}
+
 				break;
 			case 'i':
 				print_flags |= PRINT_ALL_INFO;
 				print_flags_set = 1;
-				if(dont_block == -1)
+
+				if(dont_block == -1) {
 					dont_block = 1;
+				}
+
 				break;
 			case 'l':
 				print_flags |= PRINT_LABELS;
@@ -579,38 +688,51 @@ int main(int argc, char *argv[])
 				exit(1);
 		}
 	} while (1);
-	if(dont_block == -1)
+
+	if(dont_block == -1) {
 		dont_block = 0;
+	}
 
 	if (optind + 1 == argc) {
 		device = argv[optind];
 		optind++;
 	}
+
 	if (optind != argc) {
 		usage(argc, argv);
 		exit(1);
 	}
+
 	nfds = 1;
 	ufds = calloc(1, sizeof(ufds[0]));
 	ufds[0].fd = inotify_init();
 	ufds[0].events = POLLIN;
+
 	if(device) {
-		if(!print_flags_set)
+		if(!print_flags_set) {
 			print_flags |= PRINT_DEVICE_ERRORS;
+		}
+
 		res = open_device(device, print_flags);
+
 		if(res < 0) {
 			return 1;
 		}
 	} else {
-		if(!print_flags_set)
+		if(!print_flags_set) {
 			print_flags |= PRINT_DEVICE_ERRORS | PRINT_DEVICE | PRINT_DEVICE_NAME;
+		}
+
 		print_device = 1;
 		res = inotify_add_watch(ufds[0].fd, device_path, IN_DELETE | IN_CREATE);
+
 		if(res < 0) {
 			fprintf(stderr, "could not add watch for %s, %s\n", device_path, strerror(errno));
 			return 1;
 		}
+
 		res = scan_dir(device_path, print_flags);
+
 		if(res < 0) {
 			fprintf(stderr, "scan dir failed for %s\n", device_path);
 			return 1;
@@ -621,47 +743,64 @@ int main(int argc, char *argv[])
 		for(i = 1; i < nfds; i++) {
 			uint16_t sw;
 			res = ioctl(ufds[i].fd, EVIOCGSW(1), &sw);
+
 			if(res < 0) {
 				fprintf(stderr, "could not get switch state, %s\n", strerror(errno));
 				return 1;
 			}
+
 			sw &= get_switch;
 			printf("%04x%s", sw, newline);
 		}
 	}
 
-	if(dont_block)
+	if(dont_block) {
 		return 0;
+	}
 
 	while(1) {
 		pollres = poll(ufds, nfds, -1);
+
 		//printf("poll %d, returned %d\n", nfds, pollres);
 		if(ufds[0].revents & POLLIN) {
 			read_notify(device_path, ufds[0].fd, print_flags);
 		}
+
 		for(i = 1; i < nfds; i++) {
 			if(ufds[i].revents) {
 				if(ufds[i].revents & POLLIN) {
 					res = read(ufds[i].fd, &event, sizeof(event));
+
 					if(res < (int)sizeof(event)) {
 						fprintf(stderr, "could not get event\n");
 						return 1;
 					}
+
 					if(get_time) {
 						printf("[%8ld.%06ld] ", event.time.tv_sec, event.time.tv_usec);
 					}
-					if(print_device)
+
+					if(print_device) {
 						printf("%s: ", device_names[i]);
+					}
+
 					print_event(event.type, event.code, event.value, print_flags);
+
 					if(sync_rate && event.type == 0 && event.code == 0) {
 						int64_t now = event.time.tv_sec * 1000000LL + event.time.tv_usec;
-						if(last_sync_time)
+
+						if(last_sync_time) {
 							printf(" rate %lld", 1000000LL / (now - last_sync_time));
+						}
+
 						last_sync_time = now;
 					}
+
 					printf("%s", newline);
-					if(event_count && --event_count == 0)
+
+					if(event_count && --event_count == 0) {
 						return 0;
+					}
 				}
 			}
 		}
