742a6c2a8c1fea8870019fda7dafe064235d3b00
xiaofei <alibaba22001@163.com>
2017-01-23 18:39:29 +0800

    update qt fonts experience

--- qt/project/printer/main.cpp
+++ qt/project/printer/main.cpp
@@ -5,6 +5,13 @@ int main(int argc, char *argv[])
 {
 	Application a(argc, argv);
 
+	qDebug() << a.font().rawName();
+	qDebug() << a.font().family();
+	qDebug() << a.font().defaultFamily();
+	qDebug() << a.font().styleName();
+	qDebug() << a.font().toString();
+	qDebug() << a.font().key();
+
 	MainWindow w;
 
 	if(!a.setSingleWindow(&w)) {
