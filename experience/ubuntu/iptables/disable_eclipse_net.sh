f3855c31742e87e51575109a8a627a785e841c27
xiaofei <alibaba22001@163.com>
2017-03-17 18:45:45 +0800

    update script

--- experience/ubuntu/iptables/disable_eclipse_net.sh
+++ experience/ubuntu/iptables/disable_eclipse_net.sh
@@ -4,18 +4,19 @@
 
 ips="
 8.8.8.8
+14.215.177.37
 "
 
 function set_iptables {
 	case "$1" in
 		start)
 			for ip in $ips; do
-				sudo iptables -t filter -A INPUT -d $ip/32 -p tcp --dport 80 -j REJECT
+				sudo iptables -t filter -A OUTPUT -d $ip/32 -p tcp --dport 80 -j REJECT
 			done
 			;;
 		stop)
 			for ip in $ips; do
-				sudo iptables -t filter -D INPUT -d $ip/32 -p tcp --dport 80 -j REJECT
+				sudo iptables -t filter -D OUTPUT -d $ip/32 -p tcp --dport 80 -j REJECT
 			done
 			;;
 		*)
