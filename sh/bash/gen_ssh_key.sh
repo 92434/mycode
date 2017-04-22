c5bf5c0803cdc164ae682c8a6ba65637a9b4f618
xiaofei <alibaba22001@163.com>
2017-04-23 00:05:18 +0800

    update sh

---
+++ sh/bash/gen_ssh_key.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ssh-keygen -t rsa -b 4096 -C "alibaba22001@163.com"
---
+++ sh/bash/mount_vdi.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+function load_vdi() {
+	local num="$1"
+	local vdi="$2"
+	echo "qemu-nbd -c /dev/nbd$num $vdi"
+	sudo qemu-nbd -c /dev/nbd$num "$vdi"
+}
+
+function load_vdis() {
+	local i=0
+	local vdi
+	for vdi in /media/xiaofei/DATA/Users/xiaofei/Documents/VirtualBox\ VMs/Ubuntu12.04/*.vdi;do 
+		load_vdi "$i" "$vdi"
+
+		((i++));
+	done
+	for vdi in /media/xiaofei/DATA/Users/xiaofei/Documents/VirtualBox\ VMs/WinXP/winxp/*.vdi;do 
+		load_vdi "$i" "$vdi"
+
+		((i++));
+	done
+}
+
+function mount_vdi() {
+	for i in  /dev/nbd*p*;do mkdir -p $(basename $i);done
+	for i in  /dev/nbd*p*;do sudo mount $i $(basename $i);done
+}
+
+function main() {
+	load_vdis
+	sleep 5
+	mount_vdi
+}
+
+main
