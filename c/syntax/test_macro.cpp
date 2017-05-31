c058c59394379defff05e9ba243673659c031ee8
xiaofei <alibaba22001@163.com>
2017-05-31 18:39:45 +0800

    update test macro

--- c/syntax/test_macro.cpp
+++ c/syntax/test_macro.cpp
@@ -6,14 +6,28 @@ using namespace std;
 
 #define my_print1(cp, var...) my_print0(cp, ##var)
 
+#define cond_print(cp, ...) do { \
+	if(p == 0) { \
+		my_print0("cond--0"cp, __VA_ARGS__); \
+	} else { \
+		my_print0("cond--1"cp, __VA_ARGS__); \
+	} \
+} while(0)
+
+#define my_print2(cp, ...) cond_print(cp, __VA_ARGS__)
+
+static int p = 1;
+static char xiaofei[] = "xiaofei";
+
 int main()
 {
-    my_print0("xiaofei %d\n", 3);
-    my_print1("xiaofei\n");
-    my_print0("xiaofei %d\n", 3);
+	my_print0("xiaofei %d\n", 3);
+	my_print1("xiaofei\n");
+	my_print0("xiaofei %d\n", 3);
 
-    my_print0("xiaofei %s\n", XIAOFEI);
-    return 0;
+	my_print0("xiaofei %s\n", XIAOFEI);
+
+	my_print2("xiaofei %s\n", XIAOFEI);
+	return 0;
 }
 
-static char xiaofei[] = "xiaofei";
