5ba8bbda76268dd8dce75da74130d91d61995513
xiaofei <alibaba22001@163.com>
2017-07-05 20:38:57 +0800

    update code

--- c/project/main.cpp
+++ c/project/main.cpp
@@ -6,7 +6,7 @@
  *   文件名称：main.cpp
  *   创 建 者：肖飞
  *   创建日期：2017年06月26日 星期一 18时15分41秒
- *   修改日期：2017年07月04日 星期二 19时54分29秒
+ *   修改日期：2017年07月05日 星期三 17时48分16秒
  *   描    述：
  *
  *================================================================*/
@@ -21,6 +21,7 @@
 #include <sys/un.h>
 #include <stdarg.h>
 #include <sys/time.h>
+#include <algorithm>    // std::sort
 
 typedef enum _matched_type {
 	UNKNOW = 0,
@@ -40,6 +41,11 @@ typedef enum _select_type {
 	SELECT_SAME,
 	SELECT_DIFFERENT
 } select_type_t;
+
+typedef struct _result_notifier {
+	int pid;
+	char filename[1024];
+} result_notifier_t;
 
 class task_bmp
 {
@@ -69,13 +75,17 @@ class test_task
 {
 private:
 	std::vector<task_bmp> enroll_list;
-	std::vector<task_bmp> identify_list;
+	std::vector<task_bmp> fr_identify_list;
+	std::vector<task_bmp> fa_identify_list;
 	std::set<std::string> enroll_ids;
 	std::ofstream ofs;
+	std::string logfile;
+	//int max_enroll_id_num;
 
 public:
 	test_task()
 	{
+		//max_enroll_id_num = 5;
 		clear();
 	}
 
@@ -88,8 +98,10 @@ public:
 	{
 		int ret = 0;
 		enroll_list.clear();
-		identify_list.clear();
+		fr_identify_list.clear();
+		fa_identify_list.clear();
 		enroll_ids.clear();
+		logfile.clear();
 		return ret;
 	}
 
@@ -104,7 +116,13 @@ public:
 	int add_identify_item(task_bmp bmp)
 	{
 		int ret = 0;
-		identify_list.push_back(bmp);
+
+		if(bmp.test_type == FOR_FR) {
+			fr_identify_list.push_back(bmp);
+		} else if(bmp.test_type == FOR_FA) {
+			fa_identify_list.push_back(bmp);
+		}
+
 		return ret;
 	}
 
@@ -138,10 +156,11 @@ public:
 					   (int)tv.tv_usec
 					  );
 		buffer[len] = 0;
+		logfile = buffer;
 
-		fs.mkdirs(buffer);
+		fs.mkdirs(logfile);
 
-		ofs.open(buffer);
+		ofs.open(logfile.c_str());
 
 		if(!ofs.good()) {
 			printf("open:%s failed!!! (%s)!\n", buffer, strerror(errno));
@@ -176,47 +195,146 @@ public:
 		return ret;
 	}
 
-	int start_task()
+	static bool enroll_less_than(task_bmp bmp1, task_bmp bmp2)
+	{
+		char *invalid_pos;
+		unsigned long bmp1_id, bmp2_id;
+		unsigned long bmp1_serial_no, bmp2_serial_no;
+
+		bmp1_id = strtoul(bmp1.id.c_str(), &invalid_pos, 10);
+		bmp2_id = strtoul(bmp2.id.c_str(), &invalid_pos, 10);
+
+		bmp1_serial_no = strtoul(bmp1.serial_no.c_str(), &invalid_pos, 10);
+		bmp2_serial_no = strtoul(bmp2.serial_no.c_str(), &invalid_pos, 10);
+
+		if(bmp1_id < bmp2_id) {
+			return true;
+		} else if(bmp1_id == bmp2_id) {
+			if(bmp1_serial_no < bmp2_serial_no) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	static bool identify_less_than(task_bmp bmp1, task_bmp bmp2)
+	{
+		char *invalid_pos;
+		unsigned long bmp1_id, bmp2_id;
+		unsigned long bmp1_serial_no, bmp2_serial_no;
+
+		bmp1_id = strtoul(bmp1.id.c_str(), &invalid_pos, 10);
+		bmp2_id = strtoul(bmp2.id.c_str(), &invalid_pos, 10);
+
+		bmp1_serial_no = strtoul(bmp1.serial_no.c_str(), &invalid_pos, 10);
+		bmp2_serial_no = strtoul(bmp2.serial_no.c_str(), &invalid_pos, 10);
+
+		if(bmp1_serial_no < bmp2_serial_no) {
+			return true;
+		} else if(bmp1_serial_no == bmp2_serial_no) {
+			if(bmp1_id < bmp2_id) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	int do_task()
+	{
+		int ret = 0;
+		ret  = log_file_start();
+
+		std::set<std::string>::iterator ids_it;
+
+		std::sort(enroll_list.begin(), enroll_list.end(), enroll_less_than);
+		std::sort(fr_identify_list.begin(), fr_identify_list.end(), identify_less_than);
+
+		for(ids_it = enroll_ids.begin(); ids_it != enroll_ids.end(); ids_it++) {
+			log_file("pid:%d, ppid:%d, process:%s\n", (int)getpid(), (int)getppid(), ids_it->c_str());
+		}
+
+		log_file("pid:%d, ppid:%d, enroll_list.size():%d\n", (int)getpid(), (int)getppid(), (int)enroll_list.size());
+		{
+			std::vector<task_bmp>::iterator enroll_list_it;
+
+			for(enroll_list_it = enroll_list.begin(); enroll_list_it != enroll_list.end(); enroll_list_it++) {
+				task_bmp bmp = *enroll_list_it;
+				log_file("pid:%d, ppid:%d, enroll:person:%s, id:%s, serial_no:%s\n", (int)getpid(), (int)getppid(), bmp.person.c_str(), bmp.id.c_str(), bmp.serial_no.c_str());
+			}
+		}
+		log_file("pid:%d, ppid:%d, fr_identify_list.size():%d\n", (int)getpid(), (int)getppid(), (int)fr_identify_list.size());
+		{
+			std::vector<task_bmp>::iterator identify_list_it;
+
+			for(identify_list_it = fr_identify_list.begin(); identify_list_it != fr_identify_list.end(); identify_list_it++) {
+				task_bmp bmp = *identify_list_it;
+				log_file("pid:%d, ppid:%d, identify:person:%s, id:%s, serial_no:%s\n", (int)getpid(), (int)getppid(), bmp.person.c_str(), bmp.id.c_str(), bmp.serial_no.c_str());
+			}
+		}
+		log_file("pid:%d, ppid:%d, fa_identify_list.size():%d\n", (int)getpid(), (int)getppid(), (int)fa_identify_list.size());
+		{
+			std::vector<task_bmp>::iterator identify_list_it;
+
+			for(identify_list_it = fa_identify_list.begin(); identify_list_it != fa_identify_list.end(); identify_list_it++) {
+				task_bmp bmp = *identify_list_it;
+				log_file("pid:%d, ppid:%d, identify:person:%s, id:%s, serial_no:%s\n", (int)getpid(), (int)getppid(), bmp.person.c_str(), bmp.id.c_str(), bmp.serial_no.c_str());
+			}
+		}
+
+		log_file_end();
+		return ret;
+	}
+
+	int send_client_result(std::string server_path)
+	{
+		int ret = 0;
+		/* create a socket */
+		int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+		struct sockaddr_un address;
+		result_notifier_t notifier;
+		int len = 0;
+
+		address.sun_family = AF_UNIX;
+		strcpy(address.sun_path, server_path.c_str());
+
+		/* connect to the server */
+		ret = connect(sockfd, (struct sockaddr *)&address, sizeof(address));
+
+		if(ret == -1) {
+			perror("connect failed: ");
+			exit(1);
+		}
+
+		/* exchange data */
+		notifier.pid = getpid();
+		len = snprintf(notifier.filename, 1023, "%s", logfile.c_str());
+		notifier.filename[len] = 0;
+		len = write(sockfd, &notifier, sizeof(result_notifier_t));
+
+		if(len != sizeof(result_notifier_t)) {
+			perror("send notifier failed!!!");
+		}
+
+		/* close the socket */
+		close(sockfd);
+		return ret;
+	}
+
+	int start_task(std::string server_path)
 	{
 		int ret = 0;
 
-		if(enroll_ids.size() != 0) {
+		if(enroll_ids.size() > /*max_enroll_id_num*/0) {
 			int pid = fork();
 
 			if(pid == -1) {
 			} else if(pid == 0) {
-				ret  = log_file_start();
-
-				std::set<std::string>::iterator ids_it;
-
-				for(ids_it = enroll_ids.begin(); ids_it != enroll_ids.end(); ids_it++) {
-					log_file("pid:%d, ppid:%d, process:%s\n", (int)getpid(), (int)getppid(), ids_it->c_str());
-				}
-
-				log_file("pid:%d, ppid:%d, enroll_list.size():%d\n", (int)getpid(), (int)getppid(), (int)enroll_list.size());
-				{
-					std::vector<task_bmp>::iterator enroll_list_it;
-
-					for(enroll_list_it = enroll_list.begin(); enroll_list_it != enroll_list.end(); enroll_list_it++) {
-						task_bmp bmp = *enroll_list_it;
-						log_file("pid:%d, ppid:%d, enroll:person:%s, id:%s, serial_no:%s\n", (int)getpid(), (int)getppid(), bmp.person.c_str(), bmp.id.c_str(), bmp.serial_no.c_str());
-					}
-				}
-				log_file("pid:%d, ppid:%d, identify_list.size():%d\n", (int)getpid(), (int)getppid(), (int)identify_list.size());
-				{
-					std::vector<task_bmp>::iterator identify_list_it;
-
-					for(identify_list_it = identify_list.begin(); identify_list_it != identify_list.end(); identify_list_it++) {
-						task_bmp bmp = *identify_list_it;
-						log_file("pid:%d, ppid:%d, identify:person:%s, id:%s, serial_no:%s\n", (int)getpid(), (int)getppid(), bmp.person.c_str(), bmp.id.c_str(), bmp.serial_no.c_str());
-					}
-				}
-				log_file("\n");
-				log_file("\n");
-
-				log_file_end();
-				send_client_result();
-				exit(0);
+				ret = do_task();
+				//sleep(1);
+				send_client_result(server_path);
+				exit(ret);
 			} else {
 				ret = pid;
 				return ret;
@@ -229,35 +347,6 @@ public:
 		return ret;
 	}
 
-	int send_client_result()
-	{
-		int ret = 0;
-		/* create a socket */
-		int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-
-		struct sockaddr_un address;
-		address.sun_family = AF_UNIX;
-		strcpy(address.sun_path, "server_socket");
-
-		/* connect to the server */
-		int result = connect(sockfd, (struct sockaddr *)&address, sizeof(address));
-
-		if(result == -1) {
-			perror("connect failed: ");
-			exit(1);
-		}
-
-		/* exchange data */
-		int pid = getpid();
-
-		write(sockfd, &pid, sizeof(int));
-		read(sockfd, &pid, sizeof(int));
-		//printf("get pid from server:%d(%x)\n", pid, pid);
-
-		/* close the socket */
-		close(sockfd);
-		return ret;
-	}
 };
 
 class samples_list
@@ -267,20 +356,28 @@ private:
 	std::string configure_file;
 	std::string dirname;
 	std::string enroll_pattern;
+	std::string enroll_pattern_order;
 	std::map<std::string, std::map<std::string, std::vector<task_bmp> *> *> *enroll_samples;
 
 	std::string fr_pattern;
+	std::string fr_pattern_order;
 	std::string fr_success_pattern;
+	std::string fr_success_pattern_order;
 	std::string fr_fail_pattern;
+	std::string fr_fail_pattern_order;
 	std::map<std::string, std::map<std::string, std::vector<task_bmp> *> *> *fr_samples;
 
 	std::string fa_pattern;
+	std::string fa_pattern_order;
 	std::string fa_success_pattern;
+	std::string fa_success_pattern_order;
 	std::string fa_fail_pattern;
+	std::string fa_fail_pattern_order;
 	std::map<std::string, std::map<std::string, std::vector<task_bmp> *> *> *fa_samples;
 
 	std::set<int> task_list;
 	int max_task_number;
+	std::string server_path;
 
 public:
 	samples_list()
@@ -289,7 +386,8 @@ public:
 		enroll_samples = NULL;
 		fr_samples = NULL;
 		fa_samples = NULL;
-		max_task_number = 1;
+		max_task_number = 32;
+		server_path = "/tmp/.server_socket";
 	}
 
 	int release_type_samples(std::map<std::string, std::map<std::string, std::vector<task_bmp> *> *> *samples)
@@ -429,14 +527,21 @@ public:
 				dirname = cfg.get("dir", "directory");
 
 				enroll_pattern = cfg.get("resource-pattern", "enroll-pattern");
+				enroll_pattern_order = cfg.get("resource-pattern", "enroll-pattern-order");
 
 				fr_pattern = cfg.get("resource-pattern", "fr-pattern");
+				fr_pattern_order = cfg.get("resource-pattern", "fr-pattern-order");
 				fr_success_pattern = cfg.get("resource-pattern", "fr-success-pattern");
+				fr_success_pattern_order = cfg.get("resource-pattern", "fr-success-pattern-order");
 				fr_fail_pattern = cfg.get("resource-pattern", "fr-fail-pattern");
+				fr_fail_pattern_order = cfg.get("resource-pattern", "fr-fail-pattern-order");
 
 				fa_pattern = cfg.get("resource-pattern", "fa-pattern");
+				fa_pattern_order = cfg.get("resource-pattern", "fa-pattern-order");
 				fa_success_pattern = cfg.get("resource-pattern", "fa-success-pattern");
+				fa_success_pattern_order = cfg.get("resource-pattern", "fa-success-pattern-order");
 				fa_fail_pattern = cfg.get("resource-pattern", "fa-fail-pattern");
+				fa_fail_pattern_order = cfg.get("resource-pattern", "fa-fail-pattern-order");
 			} else {
 				printf("load configuration file failed!!!\n");
 			}
@@ -448,23 +553,23 @@ public:
 				printf("no dirname!\n");
 			}
 
-			if(enroll_pattern.size()) {
+			if(enroll_pattern.size() && enroll_pattern_order.size()) {
 				have_enroll_pattern = true;
 			} else {
 				printf("no enroll_pattern!\n");
 			}
 
-			if(fr_pattern.size()) {
+			if(fr_pattern.size() && fr_pattern_order.size()) {
 				have_fr_pattern = true;
-			} else if(fr_success_pattern.size() && fr_fail_pattern.size()) {
+			} else if(fr_success_pattern.size() && fr_fail_pattern.size() && fr_success_pattern_order.size() && fr_fail_pattern_order.size()) {
 				have_fr_pattern = true;
 			} else {
 				printf("no fr pattern!\n");
 			}
 
-			if(fa_pattern.size()) {
+			if(fa_pattern.size() && fa_pattern_order.size()) {
 				have_fa_pattern = true;
-			} else if(fa_success_pattern.size() && fa_fail_pattern.size()) {
+			} else if(fa_success_pattern.size() && fa_fail_pattern.size() && fa_success_pattern_order.size() && fa_fail_pattern_order.size()) {
 				have_fa_pattern = true;
 			} else {
 				printf("no fa pattern!\n");
@@ -541,6 +646,18 @@ public:
 
 		id->push_back(bmp);
 
+		return ret;
+	}
+
+	int get_pattern_order(std::string pattern, int index) {
+		int ret = 0;
+		char c = pattern.at(index);
+		if((c >= '0') && (c <= '9')) {
+			ret = c - '0';
+		} else {
+			printf("pattern order invalid!\n");
+			exit(0);
+		}
 		return ret;
 	}
 
@@ -777,7 +894,7 @@ public:
 		server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
 		struct sockaddr_un server_addr;
 		server_addr.sun_family = AF_UNIX;
-		strcpy(server_addr.sun_path, "server_socket");
+		strcpy(server_addr.sun_path, server_path.c_str());
 
 		/* bind with the local file */
 		ret = bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
@@ -806,8 +923,8 @@ public:
 			close(server_fd);
 		}
 
-		if(access("server_socket", F_OK) == 0) {
-			unlink("server_socket");
+		if(access(server_path.c_str(), F_OK) == 0) {
+			unlink(server_path.c_str());
 		}
 
 		return ret;
@@ -819,23 +936,51 @@ public:
 		int client_sockfd;
 		struct sockaddr_un client_addr;
 		socklen_t len = sizeof(client_addr);
-		int pid;
+		result_notifier_t notifier;
 
 		client_sockfd = accept(server_fd, (struct sockaddr *)&client_addr, &len);
 
 		/* exchange data */
-		read(client_sockfd, &pid, sizeof(int));
-		//printf("get pid from client:%d(%x)\n", pid, pid);
-		write(client_sockfd, &pid, sizeof(int));
+		len = read(client_sockfd, &notifier, sizeof(result_notifier_t));
+
+		if(len != sizeof(result_notifier_t)) {
+			perror("read notifier failed!!!");
+			return ret;
+		}
+
+		printf("get log from client:pid:%d, filename:%s\n", notifier.pid, notifier.filename);
+
+		ret = notifier.pid;
 
 		/* close the socket */
 		close(client_sockfd);
 
-		ret = pid;
+		//ret = pid;
 		return ret;
 	}
 
-	int add_test_task()
+	int start_task_and_wait(test_task *task, bool wait = false)
+	{
+		int ret = 0;
+
+		ret = task->start_task(server_path);
+
+		if(ret != 0) {
+			printf("pid start:%d(%x)\n", ret, ret);
+			task_list.insert(ret);
+			task->clear();
+		}
+
+		while((task_list.size() >= max_task_number) || (wait && (task_list.size() > 0))) {
+			int pid = get_client_result();
+			printf("pid stop:%d(%x)\n", pid, pid);
+			task_list.erase(pid);
+		}
+
+		return ret;
+	}
+
+	int start_test_task()
 	{
 		int ret = 0;
 		std::map<std::string, std::map<std::string, std::vector<task_bmp> *> *>::iterator sample_it;
@@ -867,38 +1012,16 @@ public:
 					}
 
 					ret = add_test_task_person_id(fr_samples, &task, sample_it->first, person_it->first, FOR_FR, SELECT_SAME);
-					//ret = add_test_task_person_id(fa_samples, &task, sample_it->first, person_it->first, FOR_FA, SELECT_ALL);
+					ret = add_test_task_person_id(fa_samples, &task, sample_it->first, person_it->first, FOR_FA, SELECT_ALL);
 
-					ret = task.start_task();
-
-					if(ret != 0) {
-						printf("start pid:%d(%x)\n", ret, ret);
-						task_list.insert(ret);
-						task.clear();
-
-						if(task_list.size() >= max_task_number) {
-							int pid = get_client_result();
-							printf("stop pid:%d(%x)\n", pid, pid);
-							task_list.erase(pid);
-						}
-					}
 				}
+
+				start_task_and_wait(&task);
 			}
 
-			ret = task.start_task();
-
-			if(ret != 0) {
-				printf("start pid:%d(%x)\n", ret, ret);
-				task_list.insert(ret);
-				task.clear();
-			}
-
-			while(task_list.size() != 0) {
-				int pid = get_client_result();
-				printf("stop pid:%d(%x)\n", pid, pid);
-				task_list.erase(pid);
-			}
 		}
+
+		start_task_and_wait(&task, true);
 
 		return ret;
 	}
@@ -918,7 +1041,7 @@ int main(int argc, char **argv)
 	}
 
 	//samples_list.p_samples();
-	samples_list.add_test_task();
+	samples_list.start_test_task();
 
 	printf("Done!\n");
 
