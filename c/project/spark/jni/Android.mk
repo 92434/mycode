2ae9cec3f152ed0dfbad4ead791c07ad3661f7d9
xiaofei <alibaba22001@163.com>
2017-08-04 12:45:18 +0800

    update code

--- c/project/spark/jni/Android.mk
+++ c/project/spark/jni/Android.mk
@@ -6,53 +6,56 @@
 #   文件名称：Android.mk
 #   创 建 者：肖飞
 #   创建日期：2017年07月14日 星期五 16时08分37秒
-#   修改日期：2017年07月14日 星期五 16时39分59秒
+#   修改日期：2017年08月04日 星期五 12时43分33秒
 #   描    述：
 #
 #================================================================
 LOCAL_PATH := $(call my-dir)/..
-BMF_DIR := $(LOCAL_PATH)/BMF
-SRC_DIR := $(LOCAL_PATH)/algorithm
+FT_LIB_SRC_DIR := $(LOCAL_PATH)/ft_lib
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := BMFLIB
-ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-LOCAL_SRC_FILES := $(BMF_DIR)/lib/armeabi-v7a-linux-android/libBMF.a
-endif
-ifeq ($(TARGET_ARCH_ABI),x86_64)
-LOCAL_SRC_FILES := $(BMF_DIR)/lib/x86_64-linux-gnu/libBMF.a
-endif
-include $(PREBUILT_STATIC_LIBRARY)
-
-ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-include $(CLEAR_VARS)
-LOCAL_MODULE := BMFLIB_NEON
-LOCAL_SRC_FILES := $(BMF_DIR)/lib/armeabi-v7a-linux-android/libBMF_neon.a
-include $(PREBUILT_STATIC_LIBRARY)
-endif
-
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := ftalg
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/inc $(BMF_DIR)/inc
-LOCAL_CFLAGS := -Werror --debug -fstack-protector-strong -fno-tree-scev-cprop
-LOCAL_LDFLAGS := -lgcc -llog
-LOCAL_SRC_FILES := $(SRC_DIR)/focal_fp_spi.c $(SRC_DIR)/focal_sensor.c $(SRC_DIR)/fpsensorlib.c $(SRC_DIR)/focal_spa.c $(SRC_DIR)/pb_alg.c $(SRC_DIR)/timestamp.c
-LOCAL_STATIC_LIBRARIES := BMFLIB
-include $(BUILD_STATIC_LIBRARY)
-
-ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-include $(CLEAR_VARS)
-LOCAL_MODULE := ftalg_neon
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/inc $(BMF_DIR)/inc
-LOCAL_CFLAGS := -Werror --debug -fstack-protector-strong -fno-tree-scev-cprop
-LOCAL_LDFLAGS := -lgcc -llog
-LOCAL_SRC_FILES := $(SRC_DIR)/focal_fp_spi.c $(SRC_DIR)/focal_sensor.c $(SRC_DIR)/fpsensorlib.c $(SRC_DIR)/focal_spa.c $(SRC_DIR)/pb_alg.c $(SRC_DIR)/timestamp.c
-LOCAL_STATIC_LIBRARIES := BMFLIB_NEON
-include $(BUILD_STATIC_LIBRARY)
-endif
-
+ifeq ($(T),1)
 include $(CLEAR_VARS)
 LOCAL_MODULE := ft_lib
-LOCAL_SRC_FILES := $(LOCAL_PATH)/ft_lib/ft_lib.c
+LOCAL_SRC_FILES := $(FT_LIB_SRC_DIR)/FpSensorLib.c
 include $(BUILD_STATIC_LIBRARY)
+endif
+
+ifeq ($(T),2)
+include $(CLEAR_VARS)
+LOCAL_MODULE := FTLIB
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+LOCAL_SRC_FILES := $(LOCAL_PATH)/obj/local/armeabi-v7a/libft_lib.a
+endif
+ifeq ($(TARGET_ARCH_ABI),x86_64)
+LOCAL_SRC_FILES := $(LOCAL_PATH)/obj/local/x86_64/libft_lib.a
+endif
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+PROJECT := 9348
+
+app_c_files := $(LOCAL_PATH)/main.cpp
+app_c_files += $(LOCAL_PATH)/configuration.cpp
+app_c_files += $(LOCAL_PATH)/filesystem.cpp
+ifneq ($(PROJECT),)
+app_c_files += $(LOCAL_PATH)/hardware_$(PROJECT).cpp
+endif
+app_c_files += $(LOCAL_PATH)/hardware.cpp
+app_c_files += $(LOCAL_PATH)/optparse.cpp
+app_c_files += $(LOCAL_PATH)/regexp.cpp
+app_c_files += $(LOCAL_PATH)/samples_list.cpp
+app_c_files += $(LOCAL_PATH)/settings.cpp
+app_c_files += $(LOCAL_PATH)/test_task.cpp
+
+LOCAL_CFLAGS := -g
+LOCAL_CFLAGS += -Ift_lib/
+ifneq ($(PROJECT),)
+LOCAL_CFLAGS += -DPROJECT=HW$(PROJECT)
+endif
+
+#LOCAL_LDFLAGS := -Wl,-Map,main.map
+LOCAL_MODULE := spark
+LOCAL_STATIC_LIBRARIES := FTLIB
+LOCAL_SRC_FILES := $(app_c_files)
+include $(BUILD_EXECUTABLE)
+endif
