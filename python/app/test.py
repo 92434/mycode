5430da721f3b56efc88f91019bec2b70f1b11389
xiaofei <alibaba22001@163.com>
2017-10-19 18:45:35 +0800

    update code

--- python/app/test.py
+++ python/app/test.py
@@ -4,52 +4,54 @@ import sys
 import importlib
 
 class utils(object):
-	def __init__(self):
-		pass
-	def lobject(self, o):
-		print o
-		for i in dir(o):
-			print "%30s %s" %("<%s>:" %(i), type(eval("o." + i)))
+    def __init__(self):
+        pass
+    def lobject(self, o):
+        print o
+        for i in dir(o):
+            print "%30s %s" %("<%s>:" %(i), type(eval("o." + i)))
 
 def get_mod(str_mod):
-	mod=None
-	try:
-		mod = eval(str_mod)
-	except:
-		mod = None
-	mod = importlib.import_module(str_mod, mod)
-	if(mod):
-		return mod
-	try:
-		#mod = importlib.import_module(str_mod, mod)
-		mod = __import__(str_mod, globals(), locals(), [], 0)
-	except:
-		mod=None
-	return mod
+    mod=None
+    try:
+        mod = eval(str_mod)
+    except:
+        mod = None
+    mod = importlib.import_module(str_mod, mod)
+    if(mod):
+        return mod
+    try:
+        #mod = importlib.import_module(str_mod, mod)
+        mod = __import__(str_mod, globals(), locals(), [], 0)
+    except:
+        mod=None
+    return mod
 
 import unittest
 class xxx(unittest.TestCase):
-	pass
+    pass
 
 def other():
-	m = __import__('__main__')
-	for i in dir(m):
-		o = getattr(m, i)
-		if isinstance(o, type) and issubclass(o, unittest.case.TestCase):
-			print o
-			print dir(o)
+    m = __import__('__main__')
+    for i in dir(m):
+        o = getattr(m, i)
+        if isinstance(o, type) and issubclass(o, unittest.case.TestCase):
+            print o
+            print dir(o)
 
 
 def main():
-	h = utils()
-	h.lobject(get_mod(sys.argv[1]) if len(sys.argv) >= 2 else None)
+    h = utils()
+    h.lobject(get_mod(sys.argv[1]) if len(sys.argv) >= 2 else None)
 
 if __name__ == '__main__':
-	main()
-	#other()
+    main()
+    print('%s' %(sys.executable))
+    print('%s' %(sys.argv))
+    #other()
 
-	#import contextlib
-	#import urllib
-	#with contextlib.closing(urllib.urlopen("http://www.python.org/")) as front_page:
-	#	for line in front_page:
-	#		print line
+    #import contextlib
+    #import urllib
+    #with contextlib.closing(urllib.urlopen("http://www.python.org/")) as front_page:
+    #   for line in front_page:
+    #       print line
