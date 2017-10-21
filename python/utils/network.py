a87e5c65d070390b7bb35fecc70a11cefd2ce00e
xiaofei <alibaba22001@163.com>
2017-10-21 15:25:31 +0800

    update code

--- python/utils/network.py
+++ python/utils/network.py
@@ -6,18 +6,32 @@
 #   文件名称：network.py
 #   创 建 者：肖飞
 #   创建日期：2017年07月31日 星期一 12时30分28秒
-#   修改日期：2017年10月20日 星期五 09时32分19秒
+#   修改日期：2017年10月21日 星期六 14时13分42秒
 #   描    述：
 #
 #================================================================
 import six
 import cookielib
+import re
+
+import log
+logging = log.dict_configure()
+logger = logging.getLogger('default')
 
 class network(object):
     handlers = []
     urllib = six.moves.urllib
     cookie = None
     #cookie_file = 'cookie.txt'
+    fake_header = {
+        'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding' : 'gzip, deflate, br',
+        'Accept-Language' : 'zh-CN,zh;q=0.8',
+        'Cache-Control' : 'max-age=0',
+        'Connection' : 'keep-alive',
+        'Upgrade-Insecure-Requests' : '1',
+        'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/61.0.3163.79 Chrome/61.0.3163.79 Safari/537.36',
+            }
 
     def __init__(self):
         self.handlers = []
@@ -58,8 +72,6 @@ class network(object):
         #self.cookie = cookielib.MozillaCookieJar(self.cookie_file)
         #self.cookie.load()
         cookie_handler = self.urllib.request.HTTPCookieProcessor(self.cookie)  
-        for item in self.cookie:
-            print 'name:%s\t value:%s' %(item.name, item.value)
         self.handlers.append(cookie_handler)
 
     def get_cookie_by_name(self, cookie_name):
@@ -70,7 +82,7 @@ class network(object):
 
     def update_cookie_file(self):
         for item in self.cookie:
-            print 'name:%s\t value:%s' %(item.name, item.value)
+            logger.debug('name:%s\t value:%s' %(item.name, item.value))
         #self.cookie.save(ignore_discard = True, ignore_expires = True)
 
     def add_proxy_handler(self, uri):
@@ -92,46 +104,64 @@ class network(object):
             self.add_proxy_handler(i)
         self.install_opener()
 
+    def ungzip(self, data):
+        """Decompresses data for Content-Encoding: gzip.
+        """
+        from io import BytesIO
+        import gzip
+        buffer = BytesIO(data)
+        f = gzip.GzipFile(fileobj = buffer)
+        return f.read()
+
+    def undeflate(self, data):
+        """Decompresses data for Content-Encoding: deflate.
+        (the zlib compression is used.)
+        """
+        import zlib
+        decompressobj = zlib.decompressobj(-zlib.MAX_WBITS)
+        return decompressobj.decompress(data) + decompressobj.flush()
+
+    def decompresses_response_data(self, response, data):
+        content_encoding = response.info().get('Content-Encoding')
+        if content_encoding == 'gzip':
+            data = self.ungzip(data)
+        elif content_encoding == 'gzip':
+            data = self.undeflate(data)
+        return data
+
+    def r(self, pattern, text, index):
+        s = None
+        m = re.search(pattern, text)
+        if m:
+            try:
+                s = m.group(index)
+            except:
+                pass
+        return s
+
+    def decode_response_data(self, response, data):
+        content_type = response.headers.get('content-type')
+        if content_type:
+            charset = self.r(r'charset=([\w-]+)', content_type, 1)
+
+        if not charset:
+            charset = self.r(r'charset=([\w-]+)', data, 1)
+
+        if charset:
+            try:
+                data = data.decode(charset, 'ignore')
+            except:
+                logger.debug('location:%s, charset:%s' %(self.get_location(response), charset))
+        else:
+            logger.debug('location:%s, charset:%s' %(self.get_location(response), charset))
+
+        return data
+
+    def get_location(self, response):
+        return response.geturl()
+
 def main():
-    fake_headers = {
-        #'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept': '*/*',
-        'Accept-Charset': 'UTF-8,*;q=0.5',
-        'Accept-Encoding': 'gzip,deflate,sdch',
-        'Accept-Language': 'zh-CN,zh;q=0.8',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/60.0.3112.78 Chrome/60.0.3112.78 Safari/537.36'
-    }
-
-
-    post_data = None 
-
-    n = network()
-    n.default_init(0)
-
-    while not post_data:
-        c = n.get_cookie_by_name('csrftoken')
-        if c:
-            post_data = {}  
-            post_data.update({'username' : 'xiaofei'})
-            post_data.update({'password' : 'xiaofei'})
-            post_data.update({'csrfmiddlewaretoken' : '%s' %(c.value)})
-            post_data = n.urllib.parse.urlencode(post_data)  
-
-        r = n.urllib.request.Request(url = 'http://localhost:8001/admin/login/?next=/admin/', data = post_data, headers = fake_headers)
-        
-        response = n.urllib.request.urlopen(r, None)
-
-        print('%s' %(response.info()))
-
-    data = response.read()
-    if response.info().get('Content-Encoding') == 'gzip':
-        data = self.ungzip(data)
-    elif response.info().get('Content-Encoding') == 'deflate':
-        data = self.undeflate(data)
-    print('data:%s' %(data))
-
-    return response
-
+    pass
 
 if '__main__' == __name__:
     main()
