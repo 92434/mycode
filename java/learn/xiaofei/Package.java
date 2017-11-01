449fb165de4e2c36b39bca175a442a7860e9e199
xiaofei <alibaba22001@163.com>
2017-11-01 16:00:13 +0800

    updte code

--- java/learn/xiaofei/Package.java
+++ java/learn/xiaofei/Package.java
@@ -6,50 +6,54 @@ import java.net.URL;
 
 public class Package {
 
-	public String getClassName() {
-		return this.getClass().getName();
-	}
+    public String getClassName() {
+        return this.getClass().getName();
+    }
 
-	/**
-	 * @param args
-	 * @throws MalformedURLException
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String clazzName_ = new Object() {
-			public String getClassName() {
-				String clazzName = this.getClass().getName();
-				return clazzName;// .substring(0, clazzName.lastIndexOf('$'));
-			}
-		}.getClassName();
-		System.out.println(clazzName_);
-		System.out.println(new Package().getClassName());
-		System.out.println(new Package().getClass().getName());
-		System.out.println(System.getProperties());
-		try {
-			System.out.println(new URL("file:/./Package.class"));
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			System.out.println(new URL("file:./Package.class"));
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			System.out.println(new URL("file:/./*123"));
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			System.out.println(Package.class.getResource("./Package.class")
-					.openConnection().getInputStream());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    /**
+     * @param args
+     * @throws MalformedURLException
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String clazzName_ = new Object() {
+            public String getClassName() {
+                String clazzName = this.getClass().getName();
+                return clazzName;// .substring(0, clazzName.lastIndexOf('$'));
+            }
+        } .getClassName();
+        System.out.println(clazzName_);
+        System.out.println(new Package().getClassName());
+        System.out.println(new Package().getClass().getName());
+        System.out.println(System.getProperties());
+
+        try {
+            System.out.println(new URL("file:/./Package.class"));
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {
+            System.out.println(new URL("file:./Package.class"));
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {
+            System.out.println(new URL("file:/./*123"));
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {
+            System.out.println(Package.class.getResource("./Package.class")
+                               .openConnection().getInputStream());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
--- java/learn/xiaofei/Question.java
+++ java/learn/xiaofei/Question.java
@@ -3,27 +3,46 @@ package xiaofei;
 
 public class Question {
 
-	public String getClassName() {
-		return this.getClass().getName();
-	}
+    public String getClassName() {
+        return this.getClass().getName();
+    }
 
-	/**
-	 * @param args
-	 * @throws MalformedURLException
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String clazzName_ = new Object() {
-			public String getClassName() {
-				String clazzName = this.getClass().getName();
-				return clazzName;// .substring(0, clazzName.lastIndexOf('$'));
-			}
-		}.getClassName();
-		System.out.println(clazzName_);
-		System.out.println(new Question().getClassName());
-		System.out.println(new Question().getClass().getName());
-		System.out.println(System.getProperties());
-		
-		System.out.println(Integer.toString(Integer.parseInt("00f0", 16)).getClass());
-	}
+    /**
+     * @param args
+     * @throws MalformedURLException
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String clazzName_ = new Object() {
+            public String getClassName() {
+                String clazzName = this.getClass().getName();
+                return clazzName;// .substring(0, clazzName.lastIndexOf('$'));
+            }
+        } .getClassName();
+        System.out.println(clazzName_);
+        System.out.println(new Question().getClassName());
+        System.out.println(new Question().getClass().getName());
+        System.out.println(System.getProperties());
+
+        System.out.println(Integer.toString(Integer.parseInt("00f0", 16)).getClass());
+
+        byte[] strbyte = null;
+        String s_new = null;
+        String s = "肖飞";
+
+        try {
+            strbyte = s.getBytes("UTF-8");
+            s_new = new String(strbyte, "UTF-8");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        finally{
+        }
+
+		for(byte i : strbyte){
+			System.out.println(String.format("%02x", i));
+		}
+        System.out.println(s_new);
+    }
 }
