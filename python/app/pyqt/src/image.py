3173f9e69e1f6eca782a02a68705f0e8ca164944
xiaofei <alibaba22001@163.com>
2017-12-12 19:46:11 +0800

    update code

--- python/app/pyqt/src/image.py
+++ python/app/pyqt/src/image.py
@@ -6,17 +6,103 @@
 #   文件名称：image.py
 #   创 建 者：肖飞
 #   创建日期：2017年12月07日 星期四 18时09分36秒
-#   修改日期：2017年12月08日 星期五 16时01分25秒
+#   修改日期：2017年12月12日 星期二 19时39分42秒
 #   描    述：
 #
 #================================================================
-from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QListWidget, QListWidgetItem
-import sys
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QImage, QPixmap, QBrush, QColor, QPalette, QFont
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QListWidget, QListWidgetItem, QTableWidget, QTableWidgetItem
 
-def get_csv_data(index):
+import io
+import optparse
+import sys
+import time
+
+
+header1 = \
+	[
+		0x42, 0x4d,  # //file type
+		0x0, 0x0, 0x0, 0x00,  # //file size***
+		0x00, 0x00,  # //reserved
+		0x00, 0x00,  # //reserved
+		0x36, 0x4, 0x00, 0x00,  # //head byte***
+		##//infoheader
+		0x28, 0x00, 0x00, 0x00,  # //struct size
+	]
+
+header2 = \
+	[
+		0x01, 0x00,  # //must be 1
+		0x08, 0x00,  # //color count
+		0x00, 0x00, 0x00, 0x00,  # //compression
+		0x00, 0x00, 0x00, 0x00,  # //data size***
+		0x00, 0x00, 0x00, 0x00,  # //dpix
+		0x00, 0x00, 0x00, 0x00,  # //dpiy
+		0x00, 0x00, 0x00, 0x00,  # //color used
+		0x00, 0x00, 0x00, 0x00,  # //color important
+	]
+
+class FingerprintImageFile():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def addSizeInfo2FileHeader(self,buf):
+        for i in range(4):
+            buf.append((self.width >> (8 * i)) & 0xFF)
+        for i in range(4):
+            buf.append((self.height >> (8 * i)) & 0xFF)
+        return buf
+
+    def addCLUT2FileHeader(self,buf):
+        for i in range(256):
+            buf.append(i)  # r
+            buf.append(i)  # g
+            buf.append(i)  # b
+            buf.append(0)  # a
+        return buf
+
+    def generateBmp(self,buf):
+        img_buf = header1[:]
+        print('bmp buffer len:%d' %(len(buf)))
+        # add size
+        img_buf = self.addSizeInfo2FileHeader(img_buf)
+        print img_buf
+
+        img_buf = img_buf + header2
+
+        # fill color look-up table
+        img_buf = self.addCLUT2FileHeader(img_buf)
+        
+        # add data
+        for v in buf:
+            img_buf.append(ord(v))
+
+        return img_buf
+
+    def writeFile2System(self,buf, index):
+        img_buf = self.generateBmp(buf)
+        self.writeFile2FileSystem(img_buf, index)
+    
+    def writeFile2FileSystem(self,img_buf, index):
+        curTime = time.localtime(time.time())
+        fileName = time.strftime('%Y-%m-%d-%H-%M-%S',curTime)
+
+        fileName = "%s.bmp" %(index)
+
+        fBmp = open(fileName, 'wb+')
+        strBmp = ''
+        for v in img_buf:
+            strBmp += chr(v)
+        print ord(strBmp[1098])
+
+        fBmp.write(strBmp)
+        fBmp.close()    
+
+def get_csv_data(csv_file, index, package_id = None):
     d = {}
-    with open('睡眠唤醒OK.csv') as f:
+    with open(csv_file) as f:
         for i in f:
             t, pid, odata, idata = i.strip().split(',')
             l = d.get(pid)
@@ -27,12 +113,10 @@ def get_csv_data(index):
             d.update({pid : l})
     for i in d:
         l = d.get(i)
-        #print('pid:%s, len:%s' %(i, len(l)))
         if len(l) > 1024:
             print('pid:%s, len:%s' %(i, len(l)))
-            if i == '208':
+            if package_id and i == package_id:
                 l = l[1 : -4]
-                print('len:%s' %(len(l)))
                 #for j in range(0, len(l), 6):
                 #    da, db, dc, dd, de, df = l[j][1], l[j+1][1], l[j+2][1], l[j+3][1], l[j+4][1], l[j+5][1]
                 #    da = chr(int(da, 16))
@@ -54,6 +138,8 @@ def get_csv_data(index):
                     
                     xor = 0x55
                     #xor = 0xaa
+                    #xor = 0xff
+                    #xor = 0x00
                     da = chr(int(da, 16) ^ xor)
                     db = chr(int(db, 16) ^ xor)
                     dc = chr(int(dc, 16) ^ xor)
@@ -71,6 +157,8 @@ def get_csv_data(index):
 
                     d1 = chr(ord(da_l) + (ord(da_h) << 4))
                     d2 = chr(ord(db_h) + (ord(dc_l) << 4))
+                    #d1 = chr(ord(da_l) + (ord(da_h) << 4))
+                    #d2 = chr(ord(dc_h) + (ord(db_l) << 4))
 
                     #d1 = chr((ord(ld[index]) << 4))
                     #d2 = chr((ord(ld[index]) << 4))
@@ -79,15 +167,74 @@ def get_csv_data(index):
 
                     yield d1, d2
 
-def main():
-    app = QApplication(sys.argv)
+def main(argv):
+    options = optparse.OptionParser()
+    options.add_option('-f', '--file', dest='file', help='file', default=None)
+    options.add_option('-p', '--pid', dest='pid', help='file', default=None)
+    opts, args = options.parse_args(argv)
+    print('opts:%s' %(opts))
+    print('args:%s' %(args))
+    if len(args):
+        options.print_help()
+        return
+
+    if not opts.file:
+        options.print_help()
+        return
+
+    app = QApplication(argv)
     list_widget = QListWidget()
 
-    for i in range(0, 6):
+    for i in range(0, 1):
         s = ''
-        for i1, i2 in get_csv_data(i):
+        for i1, i2 in get_csv_data(opts.file, i, opts.pid):
             s += i1 + i2
-        print('len:%d', len(s))
+
+        print('len(s):%s' %(len(s)))
+
+        if len(s) != 14784:
+            continue
+
+        #table = QTableWidget(132, 112)
+        #font = QFont()
+        #font.setFamily("Calibri")
+        #font.setPointSize(6)
+        #table.setFont(font)
+
+        #table.setEditTriggers(QTableWidget.NoEditTriggers) 
+
+        #widget = QWidget()
+        #vlayout = QVBoxLayout()
+        f = io.BytesIO(s)
+        with open('%s.csv' %(opts.pid), 'wb+') as bf:
+            for h in range(0, 112):
+                #hlayout = QHBoxLayout()
+                bf_line = ''
+                for w in range(0, 132):
+                    c = f.read(1)
+                    #item = QTableWidgetItem('%02x' %(ord(c)))
+                    #item.setSizeHint(QSize(5, 5));
+                    #item.setBackground(QBrush(QColor(ord(c), ord(c), ord(c))))
+
+                    #table.setItem(h, w, item)
+                    #item = QLabel()
+                    #item.setText('%02x' %(ord(c)))
+                    #pe = QPalette()  
+                    #pe.setColor(QPalette.Background, QColor(ord(c), ord(c), ord(c)))  
+                    #item.setPalette(pe)
+                    #item.setAutoFillBackground(True)
+                    #hlayout.addWidget(item)
+
+                    bf_line += hex(ord(c)) + ','
+                #vlayout.addItem(hlayout)
+
+                bf_line += '\n'
+                bf.write(bf_line)
+        #table.resizeColumnsToContents()
+        #table.resizeRowsToContents()
+        #table.show()
+        #widget.setLayout(vlayout)
+        #widget.show()
 
         #item = QListWidgetItem(list_widget)
         #image = QImage(s, 112, 132, QImage.Format_Grayscale8)
@@ -102,6 +249,10 @@ def main():
 
         item = QListWidgetItem(list_widget)
         image = QImage(s, 132, 112, QImage.Format_Grayscale8)
+        pixmap = QPixmap.fromImage(image)
+        img = FingerprintImageFile(132, 112)
+        img.writeFile2System(s, opts.pid)
+        #pixmap.save('%d.bmp' %(i))
         widget = QWidget()
         layout = QVBoxLayout()
         label = QLabel()
@@ -109,7 +260,7 @@ def main():
         layout.addWidget(label)
 
         label = QLabel()
-        label.setPixmap(QPixmap.fromImage(image))
+        label.setPixmap(pixmap)
         layout.addWidget(label)
         widget.setLayout(layout)
         item.setSizeHint(widget.sizeHint());
@@ -119,4 +270,4 @@ def main():
     app.exec_()
 
 if '__main__' == __name__:
-    main()
+    main(sys.argv[1:])
