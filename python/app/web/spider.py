d651fa6fd7e908eb20c939605a74cc5beebe2b01
xiaofei <alibaba22001@163.com>
2017-12-24 19:32:25 +0800

    update code

--- python/app/web/spider.py
+++ python/app/web/spider.py
@@ -6,13 +6,17 @@
 #   文件名称：spider.py
 #   创 建 者：肖飞
 #   创建日期：2017年12月24日 星期日 12时41分32秒
-#   修改日期：2017年12月24日 星期日 16时28分17秒
+#   修改日期：2017年12月24日 星期日 19时31分27秒
 #   描    述：
 #
 #================================================================
 from selenium import webdriver
 from public import create_proxyauth_extension
 from pyvirtualdisplay import Display
+try:
+    import json
+except ImportError:
+    import simplejson as json
 import sys
 
 reload(sys)  
@@ -39,15 +43,23 @@ def get_page_with_proxy():
      
     driver = webdriver.Chrome(executable_path = chrome_path, chrome_options = co)
     driver.get("http://ip138.com/")
-    print(driver.page_source)
+    #print(driver.page_source)
  
 def get_page():
+    with open('.cookies.json') as f:
+        cookies = f.read()
     co = webdriver.ChromeOptions()
     co.add_argument('--proxy-server=http://127.0.0.1:8087')  
     driver = webdriver.Chrome(executable_path = chrome_path, chrome_options = co)
+    for cookie in json.loads(cookies):
+        print('cookie:%s' %(type(cookie)))
+        driver.add_cookie(cookie)
     #driver.get("http://ip138.com/")
     driver.get("https://www.google.com")
-    print(driver.page_source)
+    #print(driver.page_source)
+    for cookie in driver.get_cookies():
+        print('cookie:%s' %(type(cookie)))
+        print('cookie:%s' %(cookie))
  
 
 def main():
