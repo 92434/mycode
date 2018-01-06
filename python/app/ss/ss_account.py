75135d03c3b2a5de51d642e2a2773dec43278d9b
xiaofei <alibaba22001@163.com>
2018-01-06 13:17:04 +0800

    update code

--- python/app/ss/ss_account.py
+++ python/app/ss/ss_account.py
@@ -6,7 +6,7 @@
 #   文件名称：ss_account.py
 #   创 建 者：肖飞
 #   创建日期：2017年12月23日 星期六 09时21分51秒
-#   修改日期：2017年12月31日 星期日 18时04分58秒
+#   修改日期：2018年01月06日 星期六 13时15分01秒
 #   描    述：
 #
 #================================================================
@@ -24,88 +24,44 @@ except ImportError:
     import simplejson as json
 
 import log
-logging = log.dict_configure()
+logging = log.dict_configure(default_log_to_file = True)
 logger = logging.getLogger('default')
 
 import request
 
 r = request.request()
 
-def ishadowx_account():
-    list_accounts = []
-    url = 'https://global.ishadowx.net/'
-    data = r.request.get(url)
-    #logger.debug('data:%s' %(data))
-    html = lxml.etree.HTML(data)
-    node = html.xpath('//div[@class="portfolio-items"]/div/div[@class="portfolio-item"]/div/div[@class="hover-text"]')
-    #logger.debug('node:%s' %([(i.items(), i.text) for i in node]))
-    for node_account in node:
-        node_info = node_account.xpath('./h4')
-        account = []
-        for node_item in node_info:
-            if not node_item.text:
-                continue
-
-            item_info = node_item.xpath('./span[1]/text()')
-            if not len(item_info):
-                continue
-
-            name = '%s' %(node_item.text.strip())
-            value = '%s' %(item_info[0].strip())
-            #logger.debug('%s%s' %(name, value))
-            if name in ['IP Address:', 'Port:', 'Password:']:
-                account.append(value)
-        list_accounts.append(account)
-    #logger.debug('%s' %(list_accounts))
-    return list_accounts
-
-dict_web_addr_map = {
-        'ishadowx' : ishadowx_account,
-        }
-
-def select_list_item(list_content):
-    while True:
-        index = int(raw_input("请输入要使用的序列号:"))
-        if index < 0:
-            continue
-        if index >= len(list_content):
-            continue
-        
-        return list_content[index]
-
-def show_list_web_addr(list_web_addr):
-    row_title = ['序列号', '网址']
-    table = PrettyTable(row_title)
-    table.padding_width = 1
-    for i, addr in enumerate(list_web_addr):
-        list_row = [i, addr]
-        table.add_row(list_row)
-    print(table)
-    return select_list_item(list_web_addr)
-
-def show_list_accounts(list_accounts):
-    row_title = ['序列号', 'ip', '端口', '密码']
-    table = PrettyTable(row_title)
-    table.padding_width = 1
-    for i, account in enumerate(list_accounts):
-        list_row = [i] + account
-        table.add_row(list_row)
-    print(table)
-    return select_list_item(list_accounts)
-
-
 def b64decode(b64_content):
-    content_decoded = None
-    count = 0
-    while count < 3:
-        pad = '=' * count
-        try:
-            content_decoded = base64.b64decode(b64_content + pad)
-            break
-        except:
-            #logger.exception('')
-            count += 1
+    content_decoded = ''
+    left = len(b64_content) % 4
+    pad = '=' * (4 - left) if left != 0 else ''
+    #logger.debug('pad:%s' %(pad))
+    try:
+        content_decoded = base64.b64decode(b64_content + pad)
+    except:
+        logger.exception('')
     return content_decoded
+
+def b64decode_retry(b64_content):
+    content_decoded = ''
+    while b64_content:
+        content_decoded = b64decode(b64_content)
+        if content_decoded:
+            return content_decoded
+        else:
+            b64_content = b64_content[:-1]
+    return content_decoded
+
+def decode_utf8_retry(utf8_content):
+    while utf8_content:
+        try:
+            utf8_content = utf8_content.decode('utf-8').encode('utf-8')
+            return utf8_content
+        except:
+            utf8_content = utf8_content[:-1]
+            #logger.exception('')
+            continue
+    return utf8_content
 
 def decode_ss_link(link = ''):
     dict_account = {}
@@ -120,7 +76,7 @@ def decode_ss_link(link = ''):
         link = link.replace('ss://', '')
 
     logger.debug('link:%s' %(link))
-    decoded_link = b64decode(link)
+    decoded_link = b64decode_retry(link)
     logger.debug('decoded_link:%s' %(decoded_link))
 
     parameter_start = decoded_link.rfind('/?')
@@ -131,10 +87,11 @@ def decode_ss_link(link = ''):
         dict_account.update(item)
         parameter_start = parameter_start + len('/?')
         parameter = decoded_link[parameter_start:]
+        logger.debug('parameter:%s' %(parameter))
         list_parameter = parameter.split('&')
         for parameter_item in list_parameter:
             key, value = parameter_item.split('=')
-            value = b64decode(value)
+            value = b64decode_retry(value)
             item = {key : value}
             dict_account.update(item)
     else:
@@ -148,7 +105,7 @@ def decode_ss_link(link = ''):
         if m:
             dict_matched = m.groupdict()
             password = dict_matched.pop('password_base64')
-            password = b64decode(password)
+            password = b64decode_retry(password)
             item = {'password' : password}
             dict_matched.update(item)
             dict_account.update(dict_matched)
@@ -163,10 +120,10 @@ def decode_ss_link(link = ''):
                 item = {'protocol_param' : protocol_param}
                 dict_account.update(item)
 
-            if 'remarks' in dict_account:
-                dict_account.pop('remarks')
-            if 'group' in dict_account:
-                dict_account.pop('group')
+            #if 'remarks' in dict_account:
+            #    dict_account.pop('remarks')
+            #if 'group' in dict_account:
+            #    dict_account.pop('group')
 
             item = {'redirect' : ''}
             dict_account.update(item)
@@ -195,7 +152,93 @@ def decode_ss_link(link = ''):
     item = {'fast_open' : False}
     dict_account.update(item)
 
+    remarks = dict_account.pop('remarks')
+    remarks = decode_utf8_retry(remarks)
+    item = {'remarks' : remarks}
+    dict_account.update(item)
+
     return dict_account
+
+def select_list_item(list_content):
+    while True:
+        index = int(raw_input("请输入要使用的序列号:"))
+        if index < 0:
+            continue
+        if index >= len(list_content):
+            continue
+        
+        return list_content[index]
+
+def show_list(list_title, list_content):
+    table = PrettyTable(list_title)
+    table.padding_width = 1
+    for i, list_row in enumerate(list_content):
+        if type(list_row) != list:
+            list_row = [list_row]
+        list_row = [i] + list_row
+        table.add_row(list_row)
+    print(table)
+
+def ishadowx_list_account():
+    list_accounts = []
+    url = 'https://global.ishadowx.net/'
+    data = r.request.get(url)
+    #logger.debug('data:%s' %(data))
+    html = lxml.etree.HTML(data)
+    node = html.xpath('//div[@class="portfolio-items"]/div/div[@class="portfolio-item"]/div/div[@class="hover-text"]')
+    #logger.debug('node:%s' %([(i.items(), i.text) for i in node]))
+    for node_account in node:
+        node_info = node_account.xpath('./h4')
+        account = []
+        for node_item in node_info:
+            if not node_item.text:
+                continue
+
+            item_info = node_item.xpath('./span[1]/text()')
+            if not len(item_info):
+                continue
+
+            name = '%s' %(node_item.text.strip())
+            value = '%s' %(item_info[0].strip())
+            #logger.debug('%s%s' %(name, value))
+            if name in ['IP Address:', 'Port:', 'Password:']:
+                account.append(value)
+        list_accounts.append(account)
+    #logger.debug('%s' %(list_accounts))
+    return list_accounts
+
+def ishadowx_account():
+    list_accounts = ishadowx_list_account()
+    show_list(['序列号', 'ip', '端口', '密码'], list_accounts)
+    server, server_port, password = select_list_item(list_accounts)
+    kwargs = {}
+    item = {'server' : server}
+    kwargs.update(item)
+    item = {'server_port' : int(server_port)}
+    kwargs.update(item)
+    item = {'password' : password}
+    kwargs.update(item)
+    ss_parameter_account(**kwargs)
+
+def yahaha_account():
+    url = 'http://www.yahaha.xyz/link/g7dxNkjdaNNg53Qg?mu=0'
+    data = r.request.get(url)
+    content_decoded = b64decode_retry(data)
+    content_decoded = content_decoded.strip()
+    list_ssr = content_decoded.splitlines()
+    logger.debug('list_ssr:%s' %(list_ssr))
+    list_dict_account = []
+    #free = '0倍率'
+    for ssr_link in list_ssr:
+        dict_account = decode_ss_link(ssr_link)
+        #if not free in dict_account.pop('remarks'):
+        #    continue
+        list_dict_account.append(dict_account)
+
+    list_remarks = [account.get('remarks') for account in list_dict_account]
+    show_list(['序列号', '帐号'], list_remarks)
+    dict_account = select_list_item(list_dict_account)
+    gen_ss_conf(dict_account)
 
 def gen_ss_conf(dict_account):
     logger.debug('dict_account:%s' %(dict_account))
@@ -207,26 +250,22 @@ def ss_link_account(ss_link):
     dict_account = decode_ss_link(ss_link)
     gen_ss_conf(dict_account)
 
-def free_ss_account():
-    list_web = dict_web_addr_map.keys()
-    web_addr = show_list_web_addr(list_web)
-    account_func = dict_web_addr_map.get(web_addr)
-    list_accounts = account_func()
-
-    if not len(list_accounts):
-        logger.debug('')
-        return
-
-    server, server_port, password = show_list_accounts(list_accounts)
+def ss_parameter_account(**kwargs):
     dict_account = decode_ss_link('')
-    item = {'server' : server}
-    dict_account.update(item)
-    item = {'server_port' : int(server_port)}
-    dict_account.update(item)
-    item = {'password' : password}
-    dict_account.update(item)
+    dict_account.update(kwargs)
     gen_ss_conf(dict_account)
 
+dict_web_addr_map = {
+        'ishadowx' : ishadowx_account,
+        'yahaha' : yahaha_account,
+        }
+
+def free_ss_account():
+    list_web = dict_web_addr_map.keys()
+    show_list(['序列号', '网址'], list_web)
+    web_addr = select_list_item(list_web)
+    account_func = dict_web_addr_map.get(web_addr)
+    account_func()
 
 def main(argv):
     options = optparse.OptionParser()
