86cff272e9c89830cbc4642917e141ad2e19bd22
xiaofei <alibaba22001@163.com>
2018-01-22 10:00:40 +0800

    update code

--- python/app/ss/save.sh
+++ python/app/ss/save.sh
@@ -6,13 +6,13 @@
 #   文件名称：save.sh
 #   创 建 者：肖飞
 #   创建日期：2018年01月22日 星期一 09时37分21秒
-#   修改日期：2018年01月22日 星期一 09时39分42秒
+#   修改日期：2018年01月22日 星期一 10时00分29秒
 #   描    述：
 #
 #================================================================
 function main() {
 	if [ -f config.json ];then
-		echo mv config.json confs/config.json.$(date +%m%d.%H%M%S)
+		mv config.json confs/config.json.$(date +%m%d.%H%M%S)
 	fi
 }
 
