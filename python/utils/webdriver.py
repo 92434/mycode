f7387a1403a667a5acf8b5e2e5ec18c8ac5948e7
xiaofei <alibaba22001@163.com>
2018-01-30 13:52:22 +0800

    update

--- python/utils/webdriver.py
+++ python/utils/webdriver.py
@@ -6,7 +6,7 @@
 #   文件名称：webdriver.py
 #   创 建 者：肖飞
 #   创建日期：2018年01月27日 星期六 11时33分03秒
-#   修改日期：2018年01月30日 星期二 13时41分03秒
+#   修改日期：2018年01月30日 星期二 13时52分11秒
 #   描    述：
 #
 #================================================================
@@ -18,7 +18,6 @@ except ImportError:
     import simplejson as json
 import os
 import sys
-import threading
 import time
 
 reload(sys)  
@@ -33,54 +32,40 @@ class driver(object):
         self.co = webdriver.ChromeOptions()
         if proxy:
             self.co.add_argument('--proxy-server=%s' %(proxy))  
-        self.driver = None
         self.content = ''
-        self.timer = None
+        self.list_cookie = None
 
     def get_page(self, url, timeout = None, delay = None, list_cookies = None):
-        self.driver = webdriver.Chrome(executable_path = os.path.expanduser(self.chrome_path), chrome_options = self.co)
+        d = webdriver.Chrome(executable_path = os.path.expanduser(self.chrome_path), chrome_options = self.co)
 
         if list_cookies:
-            self.driver.get('https://www.baidu.com')
-            self.set_cookies(list_cookies)
+            d.get('https://www.baidu.com')
+            self.set_cookies(d, list_cookies)
 
         if timeout:
-            if timeout < 3:
-                timeout = 3
-            #self.start_timer(timeout - 2)
-
-            #self.driver.implicitly_wait(timeout)
-            #self.driver.set_script_timeout(timeout)
-            self.driver.set_page_load_timeout(timeout)
+            #d.implicitly_wait(timeout)
+            #d.set_script_timeout(timeout)
+            d.set_page_load_timeout(timeout)
 
         try:
-            self.driver.get(url)
+            d.get(url)
             if delay:
                 time.sleep(delay)
             #self.timer.cancel()
             #self.timer = None
-            self.content = self.driver.page_source
-            self.list_cookie = self.driver.get_cookies()
+            self.content = d.page_source
+            self.list_cookie = d.get_cookies()
         except:
             pass
         finally:
-            self.driver.quit()
-            self.driver = None
+            d.quit()
+            d = None
 
         return self.content
 
-    def timeout(self):
-        self.content = self.driver.page_source
-        self.list_cookie = self.driver.get_cookies()
-        self.timer = None
-
-    def start_timer(self, timeout):
-        self.timer = threading.Timer(timeout, self.timeout)
-        self.timer.start()
-
-    def set_cookies(self, cookies):
+    def set_cookies(self, driver, cookies):
         for cookie in cookies:
-            self.driver.add_cookie(cookie)
+            driver.add_cookie(cookie)
 
     def get_cookies(self):
         return self.list_cookie
@@ -102,7 +87,6 @@ def test_cookie():
     print('%s' %(d.content))
 
 def main():
-    import time
     test_cookie()
 
 if '__main__' == __name__:
